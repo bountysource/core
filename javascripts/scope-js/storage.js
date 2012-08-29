71072e21a6261a576f54147e5fa75836280e3855
Cory Boyd <corytheboyd@gmail.com>
2012-08-29 16:20:54 -0700

    remove methods for local and cookie storage.
    don't need the reference to old code anymore, either

--- javascripts/scope-js/storage.js
+++ javascripts/scope-js/storage.js
@@ -16,6 +16,10 @@ with (scope('Cookies')) {
     if (options.expires_at) cookie += "expires="+options.expires_at.toGMTString();
     document.cookie = cookie;
   });
+
+  define('remove', function(name) {
+    if (get(name)) document.cookie=name+"="+";path="+";expires=Thu, 01 Jan 1970 00:00:01 GMT";
+  });
 }
 
 with (scope('Local')) {
@@ -26,82 +30,10 @@ with (scope('Local')) {
   define('set', function(name, value) {
     window.localStorage.setItem(name, value);
   });
+
+  define('remove', function(name) {
+    window.localStorage.removeItem(name);
+  });
 }
 
-
-
 scope('Storage', (document.location.protocol == 'file:' || /\.dev$/.test(document.domain)) ? 'Local' : 'Cookies');
-
-// // Temporarily store data in session.
-// // Uses the Session object defined in session.js.
-// // Stores stringified data in window.name
-// Session: {
-//   // show everything that is currently saved to the session.
-//   // only really useful for debugging.
-//   inspect: function() {
-//     var sessionVars = {};
-//     var keys = Object.keys(Session).slice(1);      
-//     
-//     for (i in keys) {
-//       if (key = keys[i]) sessionVars[key] = Session[key];
-//     }
-//     
-//     return sessionVars;
-//   },
-//   
-//   // write the variable to session variables.
-//   write: function(sessvars) {
-//     for (k in sessvars) {
-//       Session[k] = sessvars[k];
-//     }
-//   },
-//   
-//   set: function(key,value) {
-//     Session[key] = value;
-//   },
-//   
-//   // read the key/value from session and return value
-//   // if multiple keys provided, returns a hash of key => value
-//   get: function() {
-//     if (arguments.length == 1) {
-//       var key = arguments[0];
-//       return Session[key];
-//     }
-//     
-//     var sessionVars = {};
-//     for (var i = 0; i < arguments.length; i++) {
-//       var key = arguments[i];
-//       sessionVars[key] = Session[key];
-//     }
-//     return sessionVars;
-//   },
-//   
-//   // delete the key/value from session and return value
-//   // if multiple keys provided, returns a hash of key => value
-//   remove: function() {
-//     if (arguments.length == 1) {
-//       var key = arguments[0];
-//       var value = Session[key];
-//       delete Session[key];
-//       return value;
-//     }
-//     
-//     var sessionVars = {};
-//     for (var i = 0; i < arguments.length; i++) {
-//       var key = arguments[i];
-//       var value = Session[key];
-//       delete Session[key];
-//       sessionVars[key] = value;
-//     }
-//     return sessionVars;
-//   },
-//   
-//   clear: function() {
-//     var keys = Object.keys(Session).slice(1);
-//     for (i in keys) {
-//       delete Session[keys[i]];
-//     }
-//   }
-// },
-
-
