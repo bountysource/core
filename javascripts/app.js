57175e8152de4de606be8455ce0cba6496829864
Warren Konkel <wkonkel@gmail.com>
2012-08-30 16:00:30 -0700

    add a few more columns

--- javascripts/app.js
+++ javascripts/app.js
@@ -47,9 +47,27 @@ with (scope('App')) {
 
     BountySource.search_repositories(term, function(repositories) {
       render({ into: target_div },
-        ul((repositories||[]).map(function(repo) {
-          return li(a({ href: '#repos/'+repo.owner+'/'+repo.name+'/issues' }, repo.owner+'/'+repo.name));
-        }))
+        table(
+          tr(
+            th('Repository'),
+            th('Bounties'),
+            th('Followers'),
+            th('Forks'),
+            th('Updated'),
+            th('Description')
+          ),
+          
+          (repositories||[]).map(function(repo) {
+            return tr(
+              td({ style: 'width: 200px' }, a({ href: '#repos/'+repo.owner+'/'+repo.name+'/issues' }, repo.owner+'/'+repo.name)),
+              td('$0.00'),
+              td(repo.followers),
+              td(repo.forks),
+              td({ style: 'white-space:nowrap' }, (repo.pushed_at||"").substr(0,10)),
+              td({ style: 'color: #888' }, repo.description)
+            );
+          })
+        )
       );
     });
   });
@@ -63,15 +81,33 @@ with (scope('App')) {
     var target_div = div('Loading...');
 
     render(
-      h2('Repository - ' + login+'/'+repository + ' - Issues'),
+      h2(login+'/'+repository + ': Issues'),
       target_div
     );
 
     BountySource.get_issues(login, repository, function(issues) {
       render({ into: target_div },
-        ul((issues||[]).map(function(issue) {
-          return li(a({ href: '#repos/'+login+'/'+repository+'/issues/'+issue.number }, issue.title));
-        }))
+        table(
+          tr(
+            th('ID'),
+            th('Title'),
+            th('Code'),
+            th('Comments'),
+            th('State'),
+            th('Updated')
+          ),
+          
+          (issues||[]).map(function(issue) {
+            return tr(
+              td(issue.number),
+              td(a({ href: '#repos/'+login+'/'+repository+'/issues/'+issue.number }, issue.title)),
+              td(issue.pull_request.diff_url ? '✔' : ''),
+              td(issue.comments),
+              td(issue.state),
+              td({ style: 'white-space:nowrap' }, (issue.updated_at||"").substr(0,10))
+            )
+          })
+        )
       );
     });
   });
@@ -86,7 +122,7 @@ with (scope('App')) {
 
     BountySource.get_issue(login, repository, issue_number, function(issue) {
       render({ into: target_div },
-        p(issue.body),
+        p({ style: 'white-space: pre' }, issue.body),
         issue.labels && issue.labels.length > 0 && div(
           h3('Lables:'),
           ul(issue.labels.map(function(label) { return li(label.name) }))
