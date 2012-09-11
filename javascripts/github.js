c0f5fcf9c7d2bf446de8c037dcc86d550ddd3d89
Cory Boyd <corytheboyd@gmail.com>
2012-09-10 18:40:44 -0700

    github auth link

--- javascripts/github.js
+++ javascripts/github.js
@@ -1,16 +1,18 @@
 with (scope('Github')) {
   // funnel through Github, to require app link if needed
-  define('link_requiring_auth', function(options) {
+  define('link_requiring_auth', function(options, text) {
     options = options || {};
-    if (Storage.get('access_token')) {
-      options.href = options.route || get_route();
-    } else {
+
+    var original_href = '#'+options.href.split('#').slice(-1)[0];
+
+    // if not logged in, save the intended href, and replace with auth url.
+    // when auth returns, pick off the href and set_route there
+    if (!Storage.get('access_token')) {
       options.href = BountySource.api_host + 'auth/github?redirect_url='+document.location.href;
-      Storage.set('__return_route__', options.route || get_route()); // redirect back to the page the link was clicked on
     }
 
-    var text = options.text;
-    delete options.text;
-    return a(options, text);
+    var the_link =  a(options, text);
+    the_link.onclick = function() { Storage.set('__return_route__', original_href) };
+    return the_link;
   });
 };
