a53992b046b73d9f80ec5cee72184f680561b5e4
Cory Boyd <corytheboyd@gmail.com>
2012-09-10 18:40:54 -0700

    fork title

--- javascripts/repositories/fork.js
+++ javascripts/repositories/fork.js
@@ -2,7 +2,15 @@ with (scope('Repository', 'App')) {
   route('#repos/:login/:repository/issues/:issue_number/fork', function(login, repository, issue_number) {
     var target_div = div('Loading...');
 
-    render(target_div);
+    render(
+      breadcrumbs(
+        a({ href: '#repos/' + login + '/' + repository + '/issues' }, login+'/'+repository),
+        a({ href: '#repos/' + login + '/' + repository + '/issues/' + issue_number }, 'Issue #' + issue_number),
+        ('Start Solution')
+      ),
+
+      target_div
+    );
 
     // if the dev has already started a solution, redirect. otherwise, allow them to create repo and branch
     Issue.get_solution(issue_number, function(solution) {
@@ -10,7 +18,6 @@ with (scope('Repository', 'App')) {
         set_route('#repos/'+login+'/'+repository+'/issues/'+issue_number);
       } else {
         render({ into: target_div },
-          h2('Start working on ', repository),
           h3('Fork Repository / Create Solution Branch.'),
           p('To collect this bounty, you must develop your solution on the branch that we create here. The repository will automatically be forked to your account.'),
           shy_form({ action: curry(create_solution, login, repository, issue_number) },
