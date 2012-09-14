7c69159ff358df9bcd45881c15ffb340698a8140
Cory Boyd <corytheboyd@gmail.com>
2012-09-13 17:02:35 -0700

    make most descriptions fit

--- javascripts/repositories/search.js
+++ javascripts/repositories/search.js
@@ -33,7 +33,7 @@ with (scope('Repository', 'App')) {
               td(repo.followers),
               td(repo.forks),
               td({ style: 'white-space:nowrap' }, (repo.pushed_at||"").substr(0,10)),
-              td({ style: 'color: #888' }, repo.description)
+              td({ style: 'color: #888' }, p({ style: 'width: 250px;' }, repo.description))
             );
           })
         )
