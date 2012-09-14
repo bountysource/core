c672704f8c991dd8d21b0995df0b28bb1433859a
Warren Konkel <wkonkel@gmail.com>
2012-09-14 11:32:25 -0700

    amazon placeholder

--- javascripts/issues/show.js
+++ javascripts/issues/show.js
@@ -110,7 +110,8 @@ with (scope('Issue', 'App')) {
           ),
           div({ 'class': 'payment-method' },
             div(radio({ name: 'payment_method', value: 'paypal', checked: 'checked', id: 'payment_method_paypal' }), label({ 'for': 'payment_method_paypal' }, img({ src: 'images/paypal.png'}), "PayPal")),
-            div(radio({ disabled: true, name: 'payment_method', value: 'google', id: 'payment_method_google' }), label({ style: 'color: #C2C2C2;', 'for': 'payment_method_google' }, img({ src: 'images/google-wallet.png'}), "Google Wallet"))
+            div(radio({ disabled: true, name: 'payment_method', value: 'google', id: 'payment_method_google' }), label({ style: 'color: #C2C2C2;', 'for': 'payment_method_google' }, img({ src: 'images/google-wallet.png'}), "Google Wallet")),
+            div(radio({ disabled: true, name: 'payment_method', value: 'amazon', id: 'payment_method_amazon' }), label({ style: 'color: #C2C2C2;', 'for': 'payment_method_amazon' }, img({ src: 'images/amazon.png'}), "Amazon.com"))
           ),
           submit({ 'class': 'blue' }, 'Create Bounty')
         )
