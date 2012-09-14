bbd9d78071e0bad85119d18dde0bda5b90268a9c
Paul Makepeace <paulm+github@paulm.com>
2012-09-13 17:39:04 -0700

    avatar_url

--- javascripts/home.js
+++ javascripts/home.js
@@ -66,7 +66,8 @@ with (scope('Home', 'App')) {
                 h2('Staff Picks'),
                 ul(
                   data.projects.featured.map(function(repo) {
-                    return li(img({ src: repo.avatar_url || default_avatar_url,
+                    console.log(repo.avatar_url);
+                    return li(img({ src: (repo.avatar_url || default_avatar_url),
                                     style: 'width: 32px; height: 32px' }),
                       a({ href: '#repos/' + repo.full_name, style: 'color: #222' }, repo.display_name))
                   })
