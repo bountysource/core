3afb5216f95b2532e33a2caf0f100600d5319785
Cory Boyd <corytheboyd@gmail.com>
2012-09-25 14:18:45 -0700

    avatar url from owner

--- javascripts/repositories/overview.js
+++ javascripts/repositories/overview.js
@@ -18,7 +18,7 @@ with (scope('Repository', 'App')) {
       render({ into: target_div },
         div({ style: 'float: left; width: 735px; margin-right: 30px' },
           section(
-            img({ src: repo.avatar_url || 'https://a248.e.akamai.net/assets.github.com/images/gravatars/gravatar-user-420.png', style: 'width: 75px; height: 75px; vertical-align: middle; margin-right: 10px; float: left'}),
+            img({ src: repo.owner.avatar_url, style: 'width: 75px; height: 75px; vertical-align: middle; margin-right: 10px; float: left'}),
             h2({ style: 'margin: 0 0 10px 0' }, repo.owner.login),
             repo.owner.login != repo.name && h2({ style: 'margin: 0 0 10px 0' }, repo.name),
             repo.description && span(repo.description),
