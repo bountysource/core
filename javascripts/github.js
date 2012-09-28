c3e01b2997653e502f189c0634b05e4928273a54
Cory Boyd <corytheboyd@gmail.com>
2012-09-27 19:48:05 -0700

    auth url for github sends bounty source access token if user logged in.

--- javascripts/github.js
+++ javascripts/github.js
@@ -1,4 +1,19 @@
 with (scope('Github')) {
+  define('auth_url', function(options) {
+    options = options || {};
+
+    var original_route = ('#'+document.location.href.split('#').slice(-1)[0]);
+    options.redirect_url = (document.location.origin+'/'+original_route);
+
+    var url = BountySource.api_host+'auth/github';
+
+    if (Storage.get('access_token')) {
+      return url + '?access_token='+Storage.get('access_token')+'&redirect_url='+options.redirect_url;
+    } else {
+      return url + '?redirect_url='+options.redirect_url;
+    }
+  });
+
   // funnel through Github, to require app link if needed
   define('link_requiring_auth', function(options, text) {
     options = options || {};
@@ -7,9 +22,7 @@ with (scope('Github')) {
 
     // if not logged in, save the intended href, and replace with auth url.
     // when auth returns, pick off the href and set_route there
-    if (!Storage.get('access_token')) {
-      options.href = BountySource.api_host + 'auth/github?redirect_url='+document.location.origin+'/'+original_route;
-    }
+    if (!Storage.get('access_token')) options.href = auth_url();
 
     var the_link =  a(options, text);
     the_link.onclick = function() { Storage.set('_redirect_to_after_login', original_route) };
