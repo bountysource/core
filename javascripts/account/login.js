bc25e8799df544e0c9c812b3d0b24623d0c90784
Cory Boyd <corytheboyd@gmail.com>
2012-10-01 18:05:10 -0700

    reset password. basic password validation

--- javascripts/account/login.js
+++ javascripts/account/login.js
@@ -18,6 +18,8 @@ with (scope('Login', 'App')) {
         'Account',
         'Login'
       ),
+
+      messages(),
       
       div({ style: 'width: 415px; height: 320px; background: #eee; border: 1px solid #ccc; float: left; margin-right: 20px; padding: 20px' },
         h2({ style: 'text-transform: uppercase; color: #5e5f5f; font-size: 21px; text-align: center; font-weight: normal; margin-bottom: 10px' }, 'Have a GitHub Account?'),
@@ -28,7 +30,7 @@ with (scope('Login', 'App')) {
         )
       ),
       
-      div({ style: 'width: 415px; height: 320px; background: #eee; border: 1px solid #ccc; float: left; padding: 20px' },
+      div({ id: 'login-div', style: 'width: 415px; height: 320px; background: #eee; border: 1px solid #ccc; float: left; padding: 20px' },
         h2({ style: 'text-transform: uppercase; color: #5e5f5f; font-size: 21px; text-align: center; font-weight: normal; margin-bottom: 10px' }, 'Have a BountySource Account?'),
         form({ action: login, style: 'width: 200px; margin: 40px auto 0' }, 
           
@@ -37,7 +39,22 @@ with (scope('Login', 'App')) {
 
           div({ style: 'color: #999' }, "PASSWORD:"),
           div({ style: 'margin-bottom: 15px' }, password({ style: 'width: 180px; border: 1px solid #ccc; font-size: 18px; line-height: 18px; padding: 0 10px; height: 40px', name: 'password', placeholder: 'abc123'})),
-          div({ style: 'margin-top: 39px' }, submit({ 'class': 'green' }, 'Login'))
+          div({ style: 'margin-top: 39px' }, submit({ 'class': 'green' }, 'Login')),
+
+          br(),
+
+          div({ style: 'text-align: center; font-size: 12px;' }, a({ href: resize_for_password_reset_form }, 'Forgot your password?'))
+        ),
+
+        div({ id: 'password-reset-form', style: 'display: none;' },
+          form({ action: reset_password, style: 'width: 200px; margin: 40px auto 0' },
+            p("Provide us with the information below, and we will send you an email with password reset instructions."),
+
+            div({ style: 'color: #999' }, "ACCOUNT EMAIL:"),
+            div({ style: 'margin-bottom: 15px' }, text({ style: 'width: 180px; border: 1px solid #ccc; font-size: 18px; line-height: 18px; padding: 0 10px; height: 40px', name: 'email', placeholder: 'john.doe@gmail.com' })),
+
+            div({ style: 'margin-top: 39px' }, submit({ 'class': 'green' }, 'Send Email'))
+          )
         )
       ),
       
@@ -51,8 +68,64 @@ with (scope('Login', 'App')) {
         Storage.set('access_token', response.data.access_token);
         set_route(Storage.remove('_redirect_to_after_login') || '#', { reload_page: true });
       } else {
-        alert('Login and password combination was not found.');
+        render_message(error_message(response.data.error));
       }
     });
+  });
+
+  define('request_password_reset', function(form_data) {
+
+  });
+
+  define('resize_for_password_reset_form', function() {
+    document.getElementById('password-reset-form').style.display = '';
+    document.getElementById('login-div').style.height = '620px';
+  });
+
+  route('#reset_password', function(code) {
+    render(
+      breadcrumbs(
+        a({ href: '#' }, 'Home'),
+        'Account',
+        'Reset Password'
+      ),
+
+      messages(),
+
+      form({ 'class': 'fancy', action: reset_password },
+        fieldset(
+          label('Account Email:'),
+          text({ name: 'email', placeholder: 'john.doe@gmail.com', value: code||'' })
+        ),
+        fieldset(
+          label('Code:'),
+          text({ name: 'code', placeholder: 'abc123456789', value: code||'' })
+        ),
+        fieldset(
+          label('New Password:'),
+          password({ name: 'new_password', placeholder: 'abc123' })
+        ),
+        fieldset(
+          label('Confirm New Password:'),
+          password({ name: 'new_password_confirmation', placeholder: 'abc123' })
+        ),
+
+        fieldset({ 'class': 'no-label' },
+          submit({ 'class': 'green' }, 'Reset Password')
+        )
+      )
+    )
+  });
+
+  define('reset_password', function(form_data) {
+    BountySource.reset_password(form_data, function(response) {
+      console.log(response);
+
+      if (response.meta.success) {
+        render_message(success_message('Password Reset!'));
+      } else {
+        render_message(error_message(response.data.error));
+      }
+    })
   });
 }
--- javascripts/api.js
+++ javascripts/api.js
@@ -59,6 +59,10 @@ with (scope('BountySource')) {
     BountySource.api('/user/change_password', 'POST', data, callback);
   });
 
+  define('reset_password', function(data, callback) {
+    BountySource.api('/user/reset_password', 'POST', data, callback);
+  });
+
   define('search_users', function(term, callback) {
     api('/github/user/search/' + term, callback);
   });
