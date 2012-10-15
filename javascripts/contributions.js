096633dd2b55c9a4a953093488c083daec67ff6a
Cory Boyd <corytheboyd@gmail.com>
2012-10-15 12:53:53 -0700

    messages for empty issue_branches and bounties

--- javascripts/contributions.js
+++ javascripts/contributions.js
@@ -14,29 +14,35 @@ with (scope('Contributions', 'App')) {
     BountySource.user_info(function(response) {
       var user_info = response.data;
 
-      render({ into: target_div },
-        table(
-          tr(
-            th(),
-            th('Project'),
-            th('Issue Status'),
-            th('Bounty Amount'),
-            th('Date')
-          ),
+      if (!user_info.bounties || user_info.bounties.length <= 0) {
+        render({ into: target_div },
+          info_message("You have not yet made any contributions or placed any bounties. ", a({ href: '#' }, "Search for something to back."))
+        );
+      } else {
+        render({ into: target_div },
+          table(
+            tr(
+              th(),
+              th('Project'),
+              th('Issue Status'),
+              th('Bounty Amount'),
+              th('Date')
+            ),
 
-          (user_info.bounties||[]).map(function(bounty) {
-            return tr({ style: 'height: 75px;' },
-              td({ style: 'width: 60px; text-align: center; vertical-align: middle;' },
-                img({ src: bounty.repository.user.avatar_url, style: 'width: 50px;' })
-              ),
-              td(bounty.repository.full_name),
-              td(bounty.issue.state),
-              td(money(bounty.amount)),
-              td(date(bounty.created_at))
-            )
-          })
-        )
-      );
+            (user_info.bounties||[]).map(function(bounty) {
+              return tr({ style: 'height: 75px;' },
+                td({ style: 'width: 60px; text-align: center; vertical-align: middle;' },
+                  img({ src: bounty.repository.user.avatar_url, style: 'width: 50px;' })
+                ),
+                td(bounty.repository.full_name),
+                td(bounty.issue.state),
+                td(money(bounty.amount)),
+                td(date(bounty.created_at))
+              )
+            })
+          )
+        );
+      }
     });
   });
 }
--- javascripts/issue_branches/index.js
+++ javascripts/issue_branches/index.js
@@ -10,38 +10,44 @@ with (scope('IssueBranch', 'App')) {
     BountySource.user_info(function(response) {
       var info = response.data;
 
-      render({ into: target_div },
-        table(
-          tr(
-            th('Issue'),
-            th('Base Repository'),
-            th('Head Repository'),
-            th('Head Branch'),
-            th('Submitted'),
-            th('Merged'),
-            th('Actions')
-          ),
+      if (info.solutions.length <= 0) {
+        render({ into: target_div },
+          info_message("You have not started work on any issue branches yet. ", a({ href: '#bounties' }, "Find something to work on."))
+        );
+      } else {
+        render({ into: target_div },
+          table(
+            tr(
+              th('Issue'),
+              th('Base Repository'),
+              th('Head Repository'),
+              th('Head Branch'),
+              th('Submitted'),
+              th('Merged'),
+              th('Actions')
+            ),
 
-          info.solutions.map(function(s) {
-            return tr(
-              td(s.issue.number),
-              td(a({ href: '#repos/'+s.base.repository.full_name+'/issues/'+s.issue.number }, s.base.repository.full_name)),
-              td(s.head.repository.full_name),
-              td(s.head.name),
-              td(!!s.pull_request+''),
-              td(!!(s.pull_request && s.pull_request.merged)+''),
+            info.solutions.map(function(s) {
+              return tr(
+                td(s.issue.number),
+                td(a({ href: '#repos/'+s.base.repository.full_name+'/issues/'+s.issue.number }, s.base.repository.full_name)),
+                td(s.head.repository.full_name),
+                td(s.head.name),
+                td(!!s.pull_request+''),
+                td(!!(s.pull_request && s.pull_request.merged)+''),
 
-              td(
-                s.pull_request ? [
-                  div({ style: 'margin: 10px 0px;' }, a({ 'class': 'blue', target: '_blank', href: '#repos/'+s.base.repository.full_name+'/issues/'+ s.issue.number+'/issue_branch' }, 'View Submission'))
-                ] : [
-                  div({ style: 'margin: 10px 0px;' }, a({ 'class': 'green', href: '#repos/'+s.base.repository.full_name+'/issues/'+ s.issue.number+'/issue_branch' }, 'View Issue Branch'))
-                ]
-              )
-            );
-          })
+                td(
+                  s.pull_request ? [
+                    div({ style: 'margin: 10px 0px;' }, a({ 'class': 'blue', target: '_blank', href: '#repos/'+s.base.repository.full_name+'/issues/'+ s.issue.number+'/issue_branch' }, 'View Submission'))
+                  ] : [
+                    div({ style: 'margin: 10px 0px;' }, a({ 'class': 'green', href: '#repos/'+s.base.repository.full_name+'/issues/'+ s.issue.number+'/issue_branch' }, 'View Issue Branch'))
+                  ]
+                )
+              );
+            })
+          )
         )
-      )
+      }
     });
   });
 };
