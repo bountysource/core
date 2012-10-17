6a085641dc0ebe031a17dbb15e37e87280e59122
Cory Boyd <corytheboyd@gmail.com>
2012-10-16 19:40:47 -0700

    sliders with pennies

--- javascripts/issue_branches/payout.js
+++ javascripts/issue_branches/payout.js
@@ -110,7 +110,7 @@ with (scope('Payout','IssueBranch')) {
     );
 
     IssueBranch.require_accepted_solution(login, repository, issue_number, target_div, function(solution, user_info) {
-      var bounty_total = parseInt(solution.issue.account_balance),
+      var bounty_total = 500,
           bounty_source_tax = parseFloat(solution.bounty_source_tax),
           project_tax = parseFloat(solution.issue.repository.project_tax);
 
@@ -131,12 +131,13 @@ with (scope('Payout','IssueBranch')) {
             ),
             fieldset(
               label('Charity:'),
-              donation_slider({ style: 'width: 500px;', name: 'charity' })
+              donation_slider({ style: 'width: 500px;', name: 'charity' }),
+              p({ style: 'font-style: italic; font-size: 12px; width: 500px;' }, "*Proceeds put into this category are all donated to our charities, which are: ", a({ href: 'https://www.eff.org/', target: '_blank' }, 'the EFF'))
             ),
             fieldset(
               label('BountySource:'),
               donation_slider({ 'data-tax': bounty_source_tax, style: 'width: 500px;', name: 'bountysource' }),
-              p({ style: 'font-style: italic; font-size: 12px; width: 350px;' }, '*BountySource receives a small portion of your earnings, but you can donate more if you want.')
+              p({ style: 'font-style: italic; font-size: 12px; width: 500px;' }, "*BountySource keeps a small portion of your earnings, but donate more if you would like.")
             )
           ),
 
@@ -165,18 +166,33 @@ with (scope('Payout','IssueBranch')) {
         donation_sliders[i].addEventListener('change', function(e) {
           // sum of donations
           var donation_sum = 0;
-          for (var j in donation_sliders) donation_sum += parseInt(donation_sliders[j].value);
-          document.getElementById('donation-total').innerHTML = money(donation_sum);
+          for (var j in donation_sliders) donation_sum += parseFloat(donation_sliders[j].value);
+          var total_cut = bounty_total - donation_sum;
 
-          // which makes your cut the difference of donations and the bounty
-          document.getElementById('total-cut').innerHTML = money(bounty_total - donation_sum);
+          // update the values
+          set_donation_total(donation_sum, bounty_total);
+          set_total_cut(total_cut, bounty_total);
 
-          // update the font sizes
-          update_font_size(document.getElementById('donation-total'), donation_sum, bounty_total)
-          update_font_size(document.getElementById('total-cut'), (bounty_total - donation_sum), bounty_total)
+          // update the font sizes, weeeee!
+          update_font_size(document.getElementById('donation-total'), donation_sum, bounty_total);
+          update_font_size(document.getElementById('total-cut'), total_cut, bounty_total);
         });
       }
     });
+  });
+
+  // don't go over max, nor under 0
+  define('set_donation_total', function(n, max) {
+    if (n < 0) n = 0;
+    if (n > max) n = max;
+    document.getElementById('donation-total').innerHTML = money(n, true);
+  });
+
+  // don't go over max, nor under 0
+  define('set_total_cut', function(n, max) {
+    if (n < 0) n = 0;
+    if (n > max) n = max;
+    document.getElementById('total-cut').innerHTML = money(n, true);
   });
 
   define('update_font_size', function(element, current, max, min_font_size, max_font_size) {
@@ -189,5 +205,143 @@ with (scope('Payout','IssueBranch')) {
     if (confirm('Are you sure?')) {
       console.log(form_data);
     }
+  });
+
+
+
+
+
+  /*
+   * Define a group of ranges. The will all scale to the same max set in the options.
+   * If you add a tax attribute to the range, it will set the min value of the range accordingly.
+   *
+   * NOTE: Make sure you add an element with id 'total-cut' somewhere. This will be updated the total amount
+   * remaining after donations/taxes are applied.
+   *
+   * Options:
+   * @data-tax -   A float, representing percentage of the max to set the min.
+   * @max -        The max value for all ranges in the group.
+   *
+   * Arguments:
+   * The remaining arguments are the name attributes of the ranges you want to include in the group
+   * */
+  define('donation_slider_group', function() {
+    var arguments =         flatten_to_array(arguments),
+      options =             shift_options_from_args(arguments),
+      difference_element =  document.getElementById(options.difference_element_id);
+
+    // collect all of the specified slider elements
+    var all_sliders = [];
+    for (var arg in arguments) all_sliders.push(document.getElementsByName(arguments[arg])[0]);
+
+    // helper function. get all the sliders in the group except for the one specified
+    var get_other_sliders = function(target_element) {
+      var other_sliders = [];
+      for (var slider in all_sliders) if (target_element != all_sliders[slider]) other_sliders.push(all_sliders[slider]);
+      return other_sliders;
+    }
+
+    // helper function. get the sum of the min values of sliders in the group
+    var sum_slider_mins = function(group) {
+      var sum = 0;
+      for (var index in group) sum += parseFloat(group[index].getAttribute('min')||0);
+      return sum;
+    }
+
+    // helper function. sum the values of the sliders in the group
+    var sum_slider_values = function(group) {
+      var sum = 0;
+      for (var index in group) sum += parseFloat(group[index].value||0);
+      return sum;
+    }
+
+    // helper function. adjust the difference view without showing negatives
+    var set_input_value = function(n, input) { input.value = formatted_number(n > 0 ? (n > options.maxmax ? options.max : n) : 0) }
+
+    // helper function. adjust the value of all sliders
+    var adjust_slider_values = function(amount, group) {
+      for (var index in group) {
+        var adjusted_value = parseFloat(group[index].value) + amount;
+        group[index].value = adjusted_value;
+        // also need to adjust the input
+        set_input_value(parseFloat(group[index].value), get_input_from_slider(group[index]));
+      }
+    }
+
+    // for each slider specified
+    for (var this_slider in all_sliders) {
+      var this_slider = all_sliders[this_slider];
+
+      // set min from tax attribute if present. also make initial value the taxed value, or zero.
+      this_slider.setAttribute('min', (this_slider.getAttribute('data-tax')) ? (parseFloat(this_slider.getAttribute('data-tax'))*options.max) : 0);
+      this_slider.setAttribute('value', this_slider.getAttribute('min'));
+
+      // default value for the input
+      get_input_from_slider(this_slider).setAttribute('value',  parseFloat(this_slider.getAttribute('value')) || 0);
+
+      // on slider change...
+      this_slider.addEventListener('change', function(e) {
+        // collect the other sliders that are not this one.
+        var other_sliders = get_other_sliders(e.target),
+          other_slider_min_sum = sum_slider_mins(other_sliders);
+
+        // add max now if missing, since it doesn't work to do it before (maybe it can, this is quicker though)
+        if (!e.target.getAttribute('max')) e.target.setAttribute('max', (options.max - other_slider_min_sum));
+
+        // when the this_slider changes, show the value in the input
+        set_input_value(e.target.value, get_input_from_slider(e.target));
+
+        // check this value + sum of the other sliders
+        var other_sliders_total = sum_slider_values(other_sliders);
+
+        // change the other slider values if need be
+        if (parseFloat(e.target.value) + other_sliders_total > options.max) {
+          // collect the other sliders with positive values
+          var other_sliders_with_value = [];
+          for (var i in other_sliders) {
+            if (parseFloat(other_sliders[i].value) > parseFloat(other_sliders[i].min)) other_sliders_with_value.push(other_sliders[i]);
+          }
+
+          // calculate the amount to step down by on other sliders
+          var amount_over = ((parseFloat(e.target.value) + other_sliders_total) - options.max);
+
+          // adjust the other sliders by that amount
+          adjust_slider_values((-1 * amount_over / (other_sliders_with_value.length)), other_sliders_with_value);
+        }
+
+        // update the sum element if it was added to the page
+        if (difference_element) set_difference_element(options.max - (other_sliders_total + parseFloat(e.target.value)));
+      });
+
+      // default value for sum element
+      if (difference_element) difference_element.innerHTML = money(options.max - sum_slider_values(all_sliders))
+    }
+
+    return all_sliders;
+  });
+
+  // HTML5 slider helper
+  define('donation_slider', function(options) {
+    var unique_bit = Math.ceil(new Date().getTime() * Math.random());
+
+    options['id']     = '_slider-'+unique_bit;
+    options['class']  = 'donation-slider';
+    options['value']  = 0;
+    options['step']   = 0.01;
+
+    var dat_range = range(options),
+        range_input = text({ id: '_input-'+unique_bit, style: 'width: 65px; color: #9C9999;', value: 0, readonly: true });
+
+    return div({ style: 'display: inline-block;' },
+      div({ style: 'margin-right: 5px; display: inline;' }, dat_range), span({ style: 'font-size: 25px; vertical-align: middle; padding-right: 5px;' }, '$'), range_input
+    );
+  });
+
+  define('get_input_from_slider', function(slider_element) {
+    return document.getElementById('_input-'+slider_element.getAttribute('id').split('-').slice(-1));
+  });
+
+  define('get_slider_from_input', function(input_element) {
+    return document.getElementById('_slider-'+input_element.getAttribute('id').split('-').slice(-1));
   });
 }
