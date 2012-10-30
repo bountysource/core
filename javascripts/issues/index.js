0b3b9a1e964078a7e2514fca3fc654ed8dbec947
Cory Boyd <corytheboyd@gmail.com>
2012-10-30 13:50:37 -0700

    issues index page

--- javascripts/issues/index.js
+++ javascripts/issues/index.js
@@ -3,11 +3,6 @@ with (scope('Issue', 'App')) {
   route('#repos/:login/:repository/issues', function(login, repository) {
     var target_div = div('Loading...');
 
-    if (!repository.has_issues) {
-      set_route('#repos/'+login+'/'+repository);
-      return;
-    }
-
     render(
       breadcrumbs(
         a({ href: '#' }, 'Home'),
@@ -19,39 +14,11 @@ with (scope('Issue', 'App')) {
     );
 
     BountySource.get_issues(login, repository, function(response) {
-      if (response.data && response.data.error) {
-        render({ into: target_div },
-          div(response.data.error)
-        );
+      if (!response.meta.success) {
+        render({ into: target_div }, error_message(response.data.error));
       } else {
-        var issues = response.data||[];
-
         render({ into: target_div },
-          Repository.issue_table({ header_class: 'thick-line-green' }, 'All Issues', issues)
-          // 
-          // table(
-          //   tr(
-          //     th('ID'),
-          //     th('Title'),
-          //     th('Bounties'),
-          //     th('Code'),
-          //     th('Comments'),
-          //     th('State'),
-          //     th('Updated')
-          //   ),
-          // 
-          //   (issues||[]).map(function(issue) {
-          //     return tr(
-          //       td(issue.number),
-          //       td(a({ href: '#repos/'+login+'/'+repository+'/issues/'+issue.number }, issue.title)),
-          //       td(money(issue.account_balance)),
-          //       td(issue.code ? '✔' : ''),
-          //       td(issue.comments),
-          //       td(issue.state),
-          //       td({ style: 'white-space:nowrap' }, (issue.updated_at||"").substr(0,10))
-          //     )
-          //   })
-          // )
+          Repository.issue_table({ header_class: 'thick-line-green' }, 'All Issues', response.data)
         );
       }
     });
