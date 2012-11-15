a6ad8c7508f998d7c52517964f7c463580eb2b6b
Cory Boyd <corytheboyd@gmail.com>
2012-11-15 11:22:36 -0800

    about me

--- javascripts/fundraisers/show.js
+++ javascripts/fundraisers/show.js
@@ -53,6 +53,14 @@ with (scope('Fundraisers')) {
           span({ style: 'font-size: 16px; margin-bottom: 35px;' }, 'by ', fundraiser.person.display_name)
         ),
 
+        fundraiser.image_url && section({ id: 'fundraiser-image', style: 'border-bottom: 2px dotted #C7C7C7;' },
+          img({ style: 'width: 100%; padding: 20px;', src: fundraiser.image_url })
+        ),
+
+        section({ id: 'fundraiser-dates' },
+          p({ style: 'text-align: right; color: #C5C5C5;' }, 'Created ', date(fundraiser.created_at))
+        ),
+
         // TODO sharing
         section({ id: 'fundraiser-share' }),
 
@@ -62,13 +70,37 @@ with (scope('Fundraisers')) {
       ),
 
       div({ 'class': 'split-side' },
-        grey_box(
+        section({ id: 'fundraiser-stats', style: 'background: #eee; padding: 10px; margin: 10px 0; border-radius: 3px;' },
           ul({ style: 'list-style-type: none; padding: 0;' },
             li({ style: 'margin: 20px auto;' },
-              span({ style: 'font-size: 45px;' }, 15+''), span({ style: 'margin-left: 5px;' }, 'backers')
+              span({ style: 'font-size: 45px; display: inline-block;' }, 15+''), br(), span({ style: 'margin-left: 5px; margin-top: 12px; display: inline-block;' }, 'backers')
             ),
             li({ style: 'margin: 20px auto;' },
-              span({ style: 'font-size: 45px;' }, money(1500)), span({ style: 'margin-left: 5px;' }, 'backers')
+              span({ style: 'font-size: 45px; display: inline-block;' }, money(1500)), br(), span({ style: 'margin-left: 5px; margin-top: 12px; display: inline-block;' }, 'pledged of ', money(fundraiser.funding_goal), ' goal')
+            )
+          )
+        ),
+
+        br(),
+
+        section({ id: 'fundraiser-about-me', style: 'background: #D2F8D2; padding: 15px; margin: 10px 0; border-radius: 3px;' },
+          div({ style: 'margin-bottom: 5px;' },
+            div({ style: 'text-align: left; display: inline-block' }, img({ src: fundraiser.person.avatar_url, style: 'width: 70px; border-radius: 3px;' })),
+            div({ style: 'display: inline-block; vertical-align: top; margin-left: 10px;' },
+              span({ style: 'font-size: 14px; color: gray;'}, 'Project by'), br(),
+              span(fundraiser.person.display_name)
+            )
+          ),
+
+          // show github profile link if provided by user
+          (fundraiser.person.github_user || true) && div({ style: 'border-top: 2px dotted #C7C7C7; padding: 5px;' },
+            img({ src: 'images/github.png', style: 'width: 30px; vertical-align: middle' }),
+            a({ style: 'vertical-align: middle; margin-left: 10px;', target: '_blank', href: 'https://github.com/'+fundraiser.person.github_user.login }, fundraiser.person.github_user.login)
+          ),
+
+          div({ style: 'border-top: 2px dotted #C7C7C7; padding-top: 10px;' },
+            div({ style: 'padding: 12px 8px; background: #B0F1B0; border-radius: 3px;' },
+              p({ style: 'white-space: pre-wrap; margin: 0;' }, fundraiser.about_me)
             )
           )
         )
