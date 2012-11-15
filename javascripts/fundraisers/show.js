6c1326d2a46862ff6569fab0da77d43861022833
Cory Boyd <corytheboyd@gmail.com>
2012-11-14 16:16:22 -0800

    preview fundraisers

--- javascripts/fundraisers/show.js
+++ javascripts/fundraisers/show.js
@@ -1,50 +1,52 @@
 with (scope('Fundraisers')) {
   route('#fundraisers/:fundraiser_id', function(fundraiser_id) {
+    var fundraiser_div = div('Loading...');
     var target_div = div(
       breadcrumbs(
         a({ href: '#' }, 'Home'),
         a({ href: '#fundraisers' }, 'Fundraisers'),
-        'Loading...'
+        span({ id: 'breadcrumbs-fundraiser-title' }, 'Loading...')
       ),
-      'Loading...'
+      fundraiser_div
     );
 
     render(target_div);
 
     BountySource.get_fundraiser(fundraiser_id, function(response) {
-      if (!response.meta.success || response.data.published) {
-        render({ into: target_div }, fundraiser_template(response.data));
+      if (response.data.published) {
+        render({ into: fundraiser_div }, fundraiser_template(response.data));
       } else {
-        render({ into: target_div }, error_message('Fundraiser not found.'));
+        render({ target: 'breadcrumbs-fundraiser-title' }, 'Oh no!');
+        render({ into: fundraiser_div }, error_message('Fundraiser not found.'));
       }
     });
   });
 
   route('#fundraisers/:fundraiser_id/preview', function(fundraiser_id) {
+    var fundraiser_div = div('Loading...');
     var target_div = div(
       breadcrumbs(
-        a({ href: '#' }, 'Home'),
-        a({ href: '#fundraisers' }, 'Fundraisers'),
-        'Loading...'
+        'Home',
+        'Fundraisers',
+        span({ id: 'breadcrumbs-fundraiser-title' }, 'Loading...')
       ),
-      'Loading...'
+      fundraiser_div
     );
 
-    render(target_div);
+    render({ layout: fundraiser_preview_layout }, target_div);
 
     BountySource.get_fundraiser(fundraiser_id, function(response) {
-      render({ into: target_div }, fundraiser_template(response.data));
+      render({ into: fundraiser_div }, fundraiser_template(response.data));
     });
   });
 
-  define('fundraiser_template', function(fundraiser) {
-    return div(
-      breadcrumbs(
-        a({ href: '#' }, 'Home'),
-        a({ href: '#fundraisers' }, 'Fundraisers'),
-        fundraiser.title
-      ),
 
+
+  define('fundraiser_template', function(fundraiser) {
+    // add the title to the already present header element
+    render({ target: 'breadcrumbs-fundraiser-title' }, fundraiser.title);
+
+    return div(
       div({ 'class': 'split-main' },
         h1(fundraiser.title),
         p(fundraiser.description)
@@ -58,5 +60,12 @@ with (scope('Fundraisers')) {
 
       div({ 'class': 'split-end' })
     );
-  })
+  });
+
+  // a layout with no navigation, for simply previewing a fundraiser
+  define('fundraiser_preview_layout', function(yield) {
+    return section({ id: 'wrapper', style: 'margin-top: 10px;' },
+      section({ id: 'content' }, yield)
+    )
+  });
 }
