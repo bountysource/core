214692e83f5c4d99dafef3ab3d3ec08167365a82
Cory Boyd <corytheboyd@gmail.com>
2012-12-14 13:37:59 -0800

    autobots, roll out

--- javascripts/home.js
+++ javascripts/home.js
@@ -132,7 +132,7 @@ with (scope('Home', 'App')) {
       // title, description, and comment count
       div({ style: 'margin: 15px 5px;' },
         div({ style: '' }, a({ href: card.href, style: 'color: inherit;' }, card.title)),
-        p({ style: 'color: #999; font-size: 80%; overflow: scroll;' }, abbreviated_text(card.description, 100)),
+        p({ style: 'color: #999; font-size: 80%; overflow: auto;' }, abbreviated_text(card.description, 100)),
         (card.comment_count > 0) && div({ style: 'text-align: right;' },
           span({ style: 'vertical-align: middle; margin-right: 5px; color: #D8A135; font-size: 16px;' }, formatted_number(card.comment_count)),  img({ style: 'vertical-align: middle;', src: 'images/icon-comments.png' })
         )
@@ -168,7 +168,7 @@ with (scope('Home', 'App')) {
 
       // title, description, and comment count
       div({ style: 'margin: 15px 5px;' },
-        p({ style: 'color: #999; overflow: scroll;' }, card.description)
+        p({ style: 'color: #999; overflow: auto;' }, card.description)
       ),
 
       div({ style: 'clear: both' }),
