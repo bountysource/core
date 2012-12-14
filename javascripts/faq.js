7e0361ee42b0c6ff9b4968f3899f2d0f06242ecb
Camille Vergara <camille@badger.com>
2012-12-14 13:59:45 -0800

    update faq

--- javascripts/faq.js
+++ javascripts/faq.js
@@ -79,7 +79,10 @@ with (scope('Faq', 'App')) {
           dd("Yes."),
     
           dt("Who can collect a bounty?"),
-          dd("Anybody who has a GitHub account.")
+          dd("Anybody who has a GitHub account."),
+          
+          dt("I'm worried about introducing money into my community."),
+          dd("As long as you continue to accept or reject code based on its merit, there shouldn't be any issues. The great thing about open-source is that, at the end of the day, it doesn't matter what someone's motivations are - code speaks for itself.")
         )
       )
     )
@@ -104,32 +107,35 @@ with (scope('Faq', 'App')) {
           dd("We don't guarantee this, but one of the main points of open-source software and making code public is to foster improvement. Committers are always monitoring pull requests, and they likely will accept any and all code they feel is of quality."),
 
           dt("How do I know if my solution has been accepted or rejected?"),
-          dd("You can view the status of each of your submissions from your ", a({ href: '#issue_branches', target: 'blank' }, "Issue Branches page"), "."),        
+          dd("We'll email you."),        
     
           dt("How do you determine who earns the bounty?"),
           dd("Once a Github issue has been closed, BountySource awards the bounty to the developer whose pull request was merged."),
         
           dt("How do I begin working on a solution?"),                                                                                                                                                                                                             
-          dd("Once you've decided on an issue you'd like to work on, simply view it on BountySource and click on 'Create Issue Branch'."),
+          dd(
+              ol(
+                li('Fork the repository.'),
+                li('Do your work.'),
+                //add 'clean merge' below
+                li('Submit a pull request on GitHub.'),
+                li('Associate this pull request with the appropriate issue on BountySource.')
+                )
+            ),
       
           dt("When does payout occur?"),
           dd("Once an issue is closed, we hold the bounty for a two-week dispute period. This is when backers and other developers can verify that the merged code does what it should. If there are no outstanding disputes at the end of this period, the winning developer can collect the bounty."),
         
           dt("How do I receive payment?"),
+          //need to add a link to payment information part of profile here
           dd("You can receive payment via Paypal or a physical check. Before collecting a bounty, you need to fill out the Payment Information portion of your profile."),
 
           dt("What does it cost to collect a bounty?"),
           dd("BountySource charges a 10% non-refundable fee upon payout. This covers costs like credit card processing fees, servers, bandwidth, and other expenses."),
         
           dt("What can I do with my earnings?"),
-          dd(
-            'What you do with your money is completely up to you. You can either collect your full 90%, or make donations:',
-            ul(
-              li(u('To charity.'), " We support charities like ", a({ href: 'https://www.eff.org/', target: 'blank' }, "EFF"), "."),
-              li(u('Back into the project. '), ' Project committers will be able to use this money to create more bounties on existing issues within the project. (That you could earn again!)')
-            )
-          ),
-        
+          dd('What you do with your money is completely up to you. You can either collect your full 90%, or make a donation to charity. We support charities like ', a({ href: 'https://www.eff.org/', target: 'blank' }, "EFF"), "."),    
+              
           dt("Do I have to pay taxes on the bounties I collect?"),
           dd("If you are in the United States and payments made to you are more than $600 for the year, we are required to issue you a Form 1099 to report the payments, which will require you to complete a Form W-9. You should consult your tax advisor as to the taxability of the payments."),
         
@@ -161,11 +167,12 @@ with (scope('Faq', 'App')) {
           dd("A bounty is money offered up as a reward for successfully resolving an open issue."),
         
           dt("Who can create a bounty?"),
-          dd("Anybody who has a PayPal or Google Wallet account."),
+          //add google wallet back
+          dd("Anybody who has a PayPal account."),
         
           dt("What can I put bounties on?"),
           dd("You can create bounties on any open issues within any GitHub project."),
-          dd("You have the option of donating directly to a project instead of an issue, but these are non-refundable. Donations to projects are made available to project committers who use this money to create bounties on issues they think are key to the project's development."),
+          //dd("You have the option of donating directly to a project instead of an issue, but these are non-refundable. Donations to projects are made available to project committers who use this money to create bounties on issues they think are key to the project's development."),
         
           dt("What happens after I post a bounty?"),
           dd("We'll let you know when a pull request is merged in relation to an issue you've backed. Also, you can view your ", a({ href: '#contributions', target: 'blank' }, "Contributions page"), " at any point to check for updates."),
@@ -182,7 +189,7 @@ with (scope('Faq', 'App')) {
 
           dt("What happens if an issue is never closed?"),
           dd("Every bounty has a six month time limit. If you created a bounty on an issue and that is not closed within six months, you will be refunded."),
-          dd("You can shorten the time limit on your bounty for a fee."),
+          //dd("You can shorten the time limit on your bounty for a fee."),
       
           dt("Does 100% of my bounty go to the developer?"),
           dd("No. BountySource charges a 10% non-refundable fee during bounty payout. Our fee covers costs like credit card processing fees, servers, bandwidth, and other expenses.")
