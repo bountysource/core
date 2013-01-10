19fb31fc93693d965cc1a9e0872510218b72c23e
Paul Makepeace <paulm+github@paulm.com>
2013-01-09 18:05:35 -0800

    Add Google Analytics key for QA

--- javascripts/google_analytics.js
+++ javascripts/google_analytics.js
@@ -1,8 +1,8 @@
-if (document.location.host == 'www.bountysource.com') {
+if (document.location.host.match(/bountysource\.com$/)) {
   var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-36879724-1']);
+  _gaq.push(['_setAccount', document.location.host.substring(0, 6) === 'www-qa' ? 'UA-37552129-1' : 'UA-36879724-1']);
 
-  (function() {
+  (function () {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
