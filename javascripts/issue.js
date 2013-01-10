807d073683881abe333cf1103a4c58055c162825
Cory Boyd <corytheboyd@gmail.com>
2013-01-10 11:51:49 -0800

    remove /

--- javascripts/issue.js
+++ javascripts/issue.js
@@ -1,7 +1,7 @@
 with (scope('Issue','App')) {
   // get the link for an issue object
   define('get_href', function(issue) {
-    return '#/repos/'+issue.repository.full_name+'/issues/'+issue.number;
+    return '#repos/'+issue.repository.full_name+'/issues/'+issue.number;
   });
 
   // render a pretty 'open' or 'closed' element
