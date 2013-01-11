e7cdd7f14bded07dd5d486fe4a5ba2e50e673fbc
Cory Boyd <corytheboyd@gmail.com>
2013-01-10 18:40:09 -0800

    credential files. api method response data override

--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -1,8 +1,9 @@
-require "rubygems"
-require 'bundler'
-Bundler.require
+require "rspec"
+require "watir-webdriver"
 
 def login_to_paypal_sandbox!
+  @master_credentials = YAML.load_file('../config/paypal.yml')["master"]
+
   # log this browser into the dev paypal account
   @browser.goto "https://developer.paypal.com/"
 
@@ -10,15 +11,18 @@ def login_to_paypal_sandbox!
   password_input  = @browser.input(id: 'login_password')
   email_input.wait_until_present
 
-  email_input.send_keys     "warren@badger.com"
-  password_input.send_keys  "p8DY3mrQ"
+  email_input.send_keys     @master_credentials["email"]
+  password_input.send_keys  @master_credentials["password"]
+
   @browser.button(value: 'Log In').click
 end
 
 def proceed_through_paypal_sandbox_flow!
+  @buyer_credentials = YAML.load_file('../config/paypal.yml')["buyer"]
+
   @browser.input(id: 'login_email').wait_until_present
-  @browser.input(id: 'login_email').send_keys     "warren_1346807229_per@badger.com"
-  @browser.input(id: 'login_password').send_keys  "346807220"
+  @browser.input(id: 'login_email').send_keys     @buyer_credentials["email"]
+  @browser.input(id: 'login_password').send_keys  @buyer_credentials["password"]
   sleep 2
   @browser.button(id: 'submitLogin').click
 
@@ -36,9 +40,11 @@ def login_with_github!
   @browser.goto 'https://github.com/login'
   # return if already logged in to GitHub
   unless @browser.div(id: 'user-links').present?
+    @github_credentials = YAML.load_file('../config/github.yml')
+
     @browser.input(id: 'login_field').wait_until_present
-    @browser.input(id: 'login_field').send_keys "bountysource-qa"
-    @browser.input(id: 'password').send_keys    "badger42"
+    @browser.input(id: 'login_field').send_keys @github_credentials["username"]
+    @browser.input(id: 'password').send_keys    @github_credentials["password"]
     @browser.input(value: 'Sign in').click
   end
 end
@@ -56,7 +62,32 @@ RSpec.configure do |config|
       end
 
       def goto_route(route)
-        goto "https://www-qa.bountysource.com/#{route}"
+        # goto "https://www-qa.bountysource.com/#{route}"
+        goto "http://www.bountysource.dev/#{route}"
+      end
+
+      # override an API method. results of block will be passed as response.data
+      def override_api_response_data(method, data, options={})
+        execute_scopejs_script %(
+          with (scope('BountySource')) {
+            define('_original_#{method}', #{method});
+            define('#{method}', function() {
+              var arguments = flatten_to_array(arguments);
+              var callback = shift_callback_from_args(arguments);
+              callback({ data: #{data.to_json}, meta: { success: #{options[:success].nil? ? true : options[:success]} } });
+            });
+          }
+        )
+      end
+
+      # restore a method that was previously mocked
+      def restore_api_method(method)
+        execute_scopejs_script %(
+          with (scope('BountySource')) {
+            define('#{method.to_s}', _original_#{method.to_s});
+            delete BountySource._original_#{method.to_s};
+          }
+        )
       end
     end
   end
