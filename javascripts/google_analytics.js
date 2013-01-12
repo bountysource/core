99344603c51fe28b303d7a963a6e36b52bf33464
Paul Makepeace <paulm+github@paulm.com>
2013-01-11 16:29:26 -0800

    Add enhanced link attribution (https://support.google.com/analytics/bin/answer.py?hl=en&utm_id=ad&answer=2558867)

--- javascripts/google_analytics.js
+++ javascripts/google_analytics.js
@@ -1,5 +1,7 @@
 if (document.location.host.match(/bountysource\.com$/)) {
   var _gaq = _gaq || [];
+  var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+  _gaq.push(['_require', 'inpage_linkid', pluginUrl]);
   _gaq.push(['_setAccount', document.location.host.substring(0, 6) === 'www-qa' ? 'UA-37552129-1' : 'UA-36879724-1']);
 
   (function () {
