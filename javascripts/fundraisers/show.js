998e2d2f5e12acf5d6d07759c8eb27268815f962
Cory Boyd <corytheboyd@gmail.com>
2013-01-16 11:49:17 -0800

    overflow: auto

--- javascripts/fundraisers/show.js
+++ javascripts/fundraisers/show.js
@@ -90,7 +90,7 @@ with (scope('Fundraisers')) {
         section({ id: 'fundraiser-share' }),
 
         section({ id: 'fundraiser-description', style: 'margin: 20px auto; padding: 10px;' },
-          div({ 'class': 'markdown', html: fundraiser.description_html, style: 'margin: 0; padding: 10px; overflow-x: scroll;' })
+          div({ 'class': 'markdown', html: fundraiser.description_html, style: 'margin: 0; padding: 10px; overflow-x: auto;' })
         )
       ),
 
