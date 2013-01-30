2dfdc9458c78d074b136e62b2225274a84a70b8f
Warren Konkel <wkonkel@gmail.com>
2013-01-29 17:04:39 -0800

    tweak utm

--- javascripts/google_analytics.js
+++ javascripts/google_analytics.js
@@ -3,15 +3,13 @@ with (scope('GoogleAnalytics')) {
     window._gaq = window._gaq || [];
     var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
     _gaq.push(['_require', 'inpage_linkid', pluginUrl]);
-    _gaq.push(['_setAccount', document.location.host === 'www.bountysource.com' ? 'UA-36879724-1' : 'UA-36879724-2']);
+    _gaq.push(['_setAccount', document.location.host == 'www.bountysource.com' ? 'UA-36879724-1' : 'UA-36879724-2']);
 
-    // allow compressed utm in params    #somepage?utm=source:medium:term:content:campaign
+    // #route?utm=source:medium:term:content:campaign
     var utm = document.location.href.match(/utm=([^&]+)/);
     if (utm) {
       utm = utm[1].split(':');
-      var src = 'utm_source='+utm[0]+'&utm_medium='+utm[1]+'&utm_term='+utm[2]+'&utm_content='+utm[3]+'&utm_campaign='+utm[4];
-      console.log(src)
-      _gaq.push(['_set', 'campaignParams', src ]);
+      _gaq.push(['_set', 'campaignParams', 'utm_source='+utm[0]+'&utm_medium='+utm[1]+'&utm_term='+utm[2]+'&utm_content='+utm[3]+'&utm_campaign='+utm[4] ]);
     }
 
     var ga = document.createElement('script'); ga.type = 'text/javascript';
