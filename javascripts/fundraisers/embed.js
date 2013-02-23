1715bebc41ef344999c1be8947b54e9110b86341
Paul Makepeace <paulm+github@paulm.com>
2013-02-22 18:52:31 -0800

    Remove debug

--- javascripts/fundraisers/embed.js
+++ javascripts/fundraisers/embed.js
@@ -1,9 +1,8 @@
 with (scope('Embed', 'Fundraiser')) {
   route('#fundraisers/:fundraiser_id/embed', function(fundraiser_id) {
-        var fundraiser_iframe = '<iframe src="' + BountySource.api_host + 'user/fundraisers/' + fundraiser_id + '/embed" ' +
-                                'style="border:0;width:250px;height:500px"></iframe>';
-        var fundraiser_textarea = textarea({ style: 'color: #ccc; font-size: 100%; white-space:nowrap;', rows: 2, width: 100 }, fundraiser_iframe);
-        console.log(fundraiser_textarea);
-        render(fundraiser_textarea);
+    var fundraiser_iframe = '<iframe src="' + BountySource.api_host + 'user/fundraisers/' + fundraiser_id + '/embed" ' +
+                            'style="border:0;width:250px;height:500px"></iframe>';
+    var fundraiser_textarea = textarea({ style: 'color: #ccc; font-size: 100%; white-space:nowrap;', rows: 2, width: 100 }, fundraiser_iframe);
+    render(fundraiser_textarea);
   });
 }
