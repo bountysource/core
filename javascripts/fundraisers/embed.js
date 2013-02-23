674776f648dc500929cf1cacfa94d7f8e46d3dca
Paul Makepeace <paulm+github@paulm.com>
2013-02-22 17:53:45 -0800

    Have backend generate embeddable HTML+CSS for fundraisers: copy behavior of fundraisers/card.js; factor out & tidy card CSS into cards.css; add fundraisers/embed controller

---
+++ javascripts/fundraisers/embed.js
@@ -0,0 +1,9 @@
+with (scope('Embed', 'Fundraiser')) {
+  route('#fundraiser/:fundraiser_id/embed', function(fundraiser_id) {
+        var fundraiser_iframe = '<iframe src="https://www.bountysource.com/user/fundraisers/embed/' + fundraiser_id + '" ' +
+                                'style="border:0;width:250px;height:500px"></iframe>';
+        var fundraiser_textarea = textarea({ style: 'color: #ccc; font-size: 100%; white-space:nowrap;', rows: 2, width: 100 }, fundraiser_iframe);
+        console.log(fundraiser_textarea);
+        render(fundraiser_textarea);
+  });
+}
