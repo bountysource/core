1feb3e335aaacf6aa001d83296b09b3e4fcd5235
Paul Makepeace <paulm+github@paulm.com>
2013-02-22 18:41:02 -0800

    Make embed routes consistent

--- javascripts/fundraisers/embed.js
+++ javascripts/fundraisers/embed.js
@@ -1,6 +1,6 @@
 with (scope('Embed', 'Fundraiser')) {
-  route('#fundraiser/:fundraiser_id/embed', function(fundraiser_id) {
-        var fundraiser_iframe = '<iframe src="' + BountySource.api_host + 'user/fundraisers/embed/' + fundraiser_id + '" ' +
+  route('#fundraisers/:fundraiser_id/embed', function(fundraiser_id) {
+        var fundraiser_iframe = '<iframe src="' + BountySource.api_host + 'user/fundraisers/' + fundraiser_id + '/embed" ' +
                                 'style="border:0;width:250px;height:500px"></iframe>';
         var fundraiser_textarea = textarea({ style: 'color: #ccc; font-size: 100%; white-space:nowrap;', rows: 2, width: 100 }, fundraiser_iframe);
         console.log(fundraiser_textarea);
