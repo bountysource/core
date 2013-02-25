48db39091acbb2edc5f025d426a445067d80a936
Paul Makepeace <paulm+github@paulm.com>
2013-02-25 11:26:17 -0800

    Tweak dimensions of iframe to zap scrollbars on win7/chrome

--- javascripts/fundraisers/embed.js
+++ javascripts/fundraisers/embed.js
@@ -1,7 +1,7 @@
 with (scope('Embed', 'Fundraiser')) {
   route('#fundraisers/:fundraiser_id/embed', function(fundraiser_id) {
     var fundraiser_iframe = '<iframe src="' + BountySource.api_host + 'user/fundraisers/' + fundraiser_id + '/embed" ' +
-                            'style="border:0;width:250px;height:500px"></iframe>';
+                            'style="border:0;width:255px;height:515px;overflow:auto"></iframe>';
     var fundraiser_textarea = textarea({ style: 'color: #ccc; font-size: 100%; white-space:nowrap;', rows: 2, width: 100 }, fundraiser_iframe);
     render(fundraiser_textarea);
   });
