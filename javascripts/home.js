b58a0d1bedd64d5ac496778f5c79ea74326636d3
Cory Boyd <corytheboyd@gmail.com>
2013-03-11 14:45:01 -0700

    cards

--- javascripts/home.js
+++ javascripts/home.js
@@ -1,5 +1,8 @@
 with (scope('Home', 'App')) {
 
+  // local cache for cards
+  define('page_state', {});
+
   route('#not_found', function() {
     render(
       h1("Oops! That page wasn't found!"),
@@ -23,9 +26,17 @@ with (scope('Home', 'App')) {
       section({ id: 'homepage' },
         Home.top_box,
 
-        div({ id: 'card-sections-wrapper' }, 'TODO: sections for cards')
+        // div({ id: 'card-sections-wrapper' }, 'TODO: sections for cards')
+
+        div({ id: 'column-container' }),
+        div({ style: 'clear: both' }),
+        div({ id: 'card-loader-div' }, 'Loading...')
       )
     );
+
+    clear_cards();
+    add_more_cards();
+    check_scroll_to_see_if_we_need_more_cards();
   });
 
   // render the homepage box, which always has recent signups at the bottom
@@ -53,5 +64,102 @@ with (scope('Home', 'App')) {
     });
 
     return recent_people_div;
+  });
+
+
+
+
+
+
+
+
+  define('clear_cards', function() {
+    // alert('clear cards');
+    page_state.current_cards = [];
+    page_state.can_load_more_cards = true;
+    render({ into: 'column-container' },
+      div({ 'class': 'card-column' }),
+      div({ 'class': 'card-column' }),
+      div({ 'class': 'card-column' }),
+      div({ 'class': 'card-column' })
+    );
+  });
+
+  define('add_more_cards', function() {
+    if (!page_state.can_load_more_cards) return;
+
+    show('card-loader-div');
+    render({ into: 'card-loader-div'}, 'Loading...');
+
+
+//    _gaq.push(['_trackEvent', 'Homepage', 'Show More Cards']);
+
+    page_state.can_load_more_cards = false;
+    BountySource.get_more_cards(page_state.current_cards, page_state.query, function(response) {
+      var col_container = document.getElementById('column-container');
+      if (!col_container) return;
+
+      if (response.data.length == 0) {
+        render({ into: 'card-loader-div' }, 'No results. Try broadening filter');
+        return;
+      }
+
+      var j, target;
+      for (j=0; j < response.data.fundraisers.length; j++) {
+        var card = response.data.fundraisers[j];
+        page_state.current_cards.push(card.card_id);
+
+        target = col_container.childNodes[0];
+        for (var k=1; k < col_container.childNodes.length; k++) {
+          if (col_container.childNodes[k].clientHeight < target.clientHeight) target = col_container.childNodes[k];
+        }
+
+        target.appendChild(Fundraiser.fundraiser_card(card));
+      }
+
+//      for (j=0; j < response.data.repositories.length; j++) {
+//        var card = response.data.repositories[j];
+//        page_state.current_cards.push(card.card_id);
+//
+//        target = col_container.childNodes[0];
+//        for (var k=1; k < col_container.childNodes.length; k++) {
+//          if (col_container.childNodes[k].clientHeight < target.clientHeight) target = col_container.childNodes[k];
+//        }
+//
+//        target.appendChild(Repository.repository_card(card));
+//
+//      }
+
+      for (j=0; j < response.data.issues.length; j++) {
+        var card = response.data.issues[j];
+        page_state.current_cards.push(card.card_id);
+
+        target = col_container.childNodes[0];
+        for (var k=1; k < col_container.childNodes.length; k++) {
+          if (col_container.childNodes[k].clientHeight < target.clientHeight) target = col_container.childNodes[k];
+        }
+
+        target.appendChild(Issue.issue_card(card));
+
+      }
+
+      hide('card-loader-div');
+
+      // if we didn't get a full response, give up
+      var card_count = response.data.fundraisers.length + response.data.issues.length;
+      if (card_count >= 50) page_state.can_load_more_cards = true;
+    });
+  });
+
+
+  define('check_scroll_to_see_if_we_need_more_cards', function() {
+    if (get_route() != '#') return;
+
+    var pageHeight = document.documentElement.scrollHeight;
+    var clientHeight = document.documentElement.clientHeight;
+    var scrollPos = document.documentElement.scrollTop || window.pageYOffset;
+    if ((pageHeight - (scrollPos + clientHeight) < 100)) add_more_cards();
+
+    setTimeout(check_scroll_to_see_if_we_need_more_cards, 500);
   });
 }
