e5a8cd0f9621c86dce3868077ae879d4ffe956c3
Cory Boyd <corytheboyd@gmail.com>
2013-03-13 14:30:17 -0700

    preview card on embed page

--- javascripts/fundraisers/embed.js
+++ javascripts/fundraisers/embed.js
@@ -1,8 +1,21 @@
 with (scope('Embed', 'Fundraiser')) {
   route('#fundraisers/:fundraiser_id/embed', function(fundraiser_id) {
     var fundraiser_iframe = '<iframe src="' + BountySource.api_host + 'user/fundraisers/' + fundraiser_id + '/embed" ' +
-                            'style="border:0;width:255px;height:515px;overflow:auto"></iframe>';
+                            'style="border:0;width:238px;height:402px;overflow:hidden"></iframe>';
     var fundraiser_textarea = textarea({ style: 'color: #ccc; font-size: 100%; white-space:nowrap;', rows: 2, width: 100 }, fundraiser_iframe);
-    render(fundraiser_textarea);
+
+
+    var iframe_src = BountySource.api_host + "user/fundraisers/" + fundraiser_id + "/embed";
+
+    render(
+      fundraiser_textarea,
+
+      br,
+
+      iframe({
+        src: iframe_src,
+        style: 'width: 238px; height: 402px; overflow: hidden; border: 0;'
+      })
+    );
   });
 }
