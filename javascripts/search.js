a46e2a56a4b265eb9b9c880b9f1ee110efbe6803
Cory Boyd <corytheboyd@gmail.com>
2013-03-27 10:48:38 -0700

    404 page for github trackers/issues that don't exist

--- javascripts/search.js
+++ javascripts/search.js
@@ -32,70 +32,80 @@ with (scope('Search', 'App')) {
     );
 
     BountySource.search(form_data.query, function(response) {
-      var results = response.data||[];
 
-      if (results.redirect_to) {
-        set_route(results.redirect_to)
-      } else if (results.create_issue) {
-        set_route('#issues/new?url=' + encodeURIComponent(form_data.query));
-      } else {
-        render({ into: target_div },
+      if (response.meta.success) {
+        var results = response.data||[];
 
-          Columns.create(),
+        if (results.redirect_to) {
+          set_route(results.redirect_to)
+        } else if (results.create_issue) {
+          set_route('#issues/new?url=' + encodeURIComponent(form_data.query));
+        } else {
+          render({ into: target_div },
 
-          Columns.main(
-            results.trackers && div(
-              h1('Projects Matching Your Search'),
-              table(
-                tr(
-                  th(),
-                  th({ style: "width: 150px" }, 'Name'),
-                  th('Description')
-                ),
+            Columns.create(),
 
-                results.trackers.map(function(tracker) {
-                  return tr(
-                    td(tracker.image_url && img({ src: tracker.image_url, style: 'width: 50px; height: 50px' })),
-                    td(a({ href: tracker.frontend_path }, tracker.name)),
-                    td(tracker.description)
-                  );
-                })
+            Columns.main(
+              results.trackers && div(
+                h1('Projects Matching Your Search'),
+                table(
+                  tr(
+                    th(),
+                    th({ style: "width: 150px" }, 'Name'),
+                    th('Description')
+                  ),
+
+                  results.trackers.map(function(tracker) {
+                    return tr(
+                      td(tracker.image_url && img({ src: tracker.image_url, style: 'width: 50px; height: 50px' })),
+                      td(a({ href: tracker.frontend_path }, tracker.name)),
+                      td(tracker.description)
+                    );
+                  })
+                )
+              ),
+
+              results.issues && div(
+                h1('Issues Matching Your Search'),
+                table(
+                  tr(
+                    th('Project'),
+                    th('Title'),
+                    th('Bounties')
+                  ),
+
+                  results.issues.map(function(issue) {
+                    return tr(
+                      td(a({ href: issue.tracker.frontend_path }, issue.tracker.name)),
+                      td(a({ href: issue.frontend_path }, issue.title)),
+                      td(money(issue.bounty_total))
+                    );
+                  })
+                )
               )
             ),
 
-            results.issues && div(
-              h1('Issues Matching Your Search'),
-              table(
-                tr(
-                  th('Project'),
-                  th('Title'),
-                  th('Bounties')
-                ),
-
-                results.issues.map(function(issue) {
-                  return tr(
-                    td(a({ href: issue.tracker.frontend_path }, issue.tracker.name)),
-                    td(a({ href: issue.frontend_path }, issue.title)),
-                    td(money(issue.bounty_total))
-                  );
-                })
+            Columns.side(
+              div({ 'class': 'info-message' },
+                p(b("Don't see what you want?")),
+                p("If you don't see the issue you're looking for, you can quickly add it to our database."),
+                div({ style: 'text-align: center;' },
+                  a({ href: '#issues/new', 'class': 'button blue' }, "Add Issue by URL")
+                )
               )
             )
-          ),
 
-          Columns.side(
-            div({ 'class': 'info-message' },
-              p(b("Don't see what you want?")),
-              p("If you don't see the issue you're looking for, you can quickly add it to our database."),
-              div({ style: 'text-align: center;' },
-                a({ href: '#issues/new', 'class': 'button blue' }, "Add Issue by URL")
-              )
-            )
+          );
+        }
+      } else {
+        render({ into: target_div },
+          h1("No results"),
+          p(
+            "We were unable to find anything. If you were expecting to see something, please send us an email: ",
+            a({ href: 'mailto:support@bountysource.com' }, 'mailto:support@bountysource.com')
           )
-
         );
       }
-
     });
   });
 };
