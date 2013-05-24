233ae82a9e7357d6ad942ad08e034534c86d179c
Cory Boyd <corytheboyd@gmail.com>
2013-05-23 17:15:50 -0700

    update anon flag for bounties and pledges on #contributions

--- javascripts/contributions.js
+++ javascripts/contributions.js
@@ -27,7 +27,8 @@ with (scope('Contributions', 'App')) {
                 th({ style: 'width: 300px;' }, 'Fundraiser'),
                 th('Pledge Amount'),
                 th('Date'),
-                th({ style: 'width: 175px; text-align: center;' })
+                th({ style: 'width: 175px; text-align: center;' }),
+                th({ style: 'width: 100px; text-align: center;' })
               ),
 
               pledges.map(function(pledge) {
@@ -38,7 +39,8 @@ with (scope('Contributions', 'App')) {
                   td(a({ href: pledge.fundraiser.frontend_path }, pledge.fundraiser.title)),
                   td(money(pledge.amount)),
                   td(formatted_date(pledge.created_at)),
-                  td({ style: 'width: 175px; text-align: center;' }, Fundraiser.pledge_status_element(pledge))
+                  td({ style: 'width: 175px; text-align: center;' }, Fundraiser.pledge_status_element(pledge)),
+                  td({ style: 'text-align: center;' }, publicize_pledge_button(pledge))
                 )
               })
             )
@@ -53,7 +55,8 @@ with (scope('Contributions', 'App')) {
                 th({ style: 'width: 260px;' }, 'Issue'),
                 th({ style: 'text-align: center;' }, 'Issue Status'),
                 th('Bounty Amount'),
-                th('Date')
+                th('Date'),
+                th({ style: 'width: 100px; text-align: center;' })
               ),
 
               bounties.map(function(bounty) {
@@ -65,7 +68,8 @@ with (scope('Contributions', 'App')) {
                   td(a({ href: Issue.get_href(bounty.issue) }, bounty.issue.title )),
                   td({ style: 'text-align: center;' }, Issue.status_element(bounty.issue)),
                   td(money(bounty.amount)),
-                  td(formatted_date(bounty.created_at))
+                  td(formatted_date(bounty.created_at)),
+                  td({ style: 'width: 100px; text-align: center;' }, publicize_bounty_button(bounty))
                 )
               })
             )
@@ -76,5 +80,99 @@ with (scope('Contributions', 'App')) {
       }
     });
   });
+
+
+  /*
+   * Publicize Pledge button stuff
+   * */
+
+  define('publicize_pledge_button', function(pledge) {
+    var container_div = div({ style: 'display: inline-block; vertical-align: top;', id: 'publicize-pledge-button-container-'+pledge.id });
+    if (pledge.anonymous) {
+      render({ into: container_div }, publicize_pledge_link_element(pledge));
+    } else {
+      render({ into: container_div }, unpublicize_pledge_link_element(pledge));
+    }
+    return container_div;
+  });
+
+  define('publicize_pledge_link_element', function(pledge) {
+    return a({
+      href: curry(publicize_pledge, pledge),
+      style: 'display: inline-block; vertical-align: middle; padding: 0; width: 80px; font-size: 12px; text-decoration: none;',
+      'class': 'button grey'
+    }, 'Public');
+  });
+
+  define('unpublicize_pledge_link_element', function(pledge) {
+    return a({
+      href: curry(unpublicize_pledge, pledge),
+      style: 'display: inline-block; vertical-align: middle; padding: 0; width: 80px; font-size: 12px; text-decoration: none;',
+      'class': 'button green'
+    }, 'Public');
+  });
+
+  define('publicize_pledge', function(pledge) {
+    BountySource.api('/user/pledges/'+pledge.id, 'PUT', { anonymous: 0 }, function(response) {
+      if (response.meta.success) {
+        render({ target: 'publicize-pledge-button-container-'+pledge.id }, unpublicize_pledge_link_element(pledge));
+      }
+    });
+  });
+
+  define('unpublicize_pledge', function(pledge) {
+    BountySource.api('/user/pledges/'+pledge.id, 'PUT', { anonymous: 1 }, function(response) {
+      if (response.meta.success) {
+        render({ target: 'publicize-pledge-button-container-'+pledge.id }, publicize_pledge_link_element(pledge));
+      }
+    });
+  });
+
+  /*
+  * Publicize Bounty button stuff
+  * */
+
+  define('publicize_bounty_button', function(bounty) {
+    var container_div = div({ style: 'display: inline-block; vertical-align: top;', id: 'publicize-bounty-button-container-'+bounty.id });
+    if (bounty.anonymous) {
+      render({ into: container_div }, publicize_bounty_link_element(bounty));
+    } else {
+      render({ into: container_div }, unpublicize_bounty_link_element(bounty));
+    }
+    return container_div;
+  });
+
+  define('publicize_bounty_link_element', function(bounty) {
+    return a({
+      href: curry(publicize_bounty, bounty),
+      style: 'display: inline-block; vertical-align: middle; padding: 0; width: 80px; font-size: 12px; text-decoration: none;',
+      'class': 'button grey'
+    }, 'Public');
+  });
+
+  define('unpublicize_bounty_link_element', function(bounty) {
+    return a({
+      href: curry(unpublicize_bounty, bounty),
+      style: 'display: inline-block; vertical-align: middle; padding: 0; width: 80px; font-size: 12px; text-decoration: none;',
+      'class': 'button green'
+    }, 'Public');
+  });
+
+  define('publicize_bounty', function(bounty) {
+    BountySource.api('/user/bounties/'+bounty.id, 'PUT', { anonymous: 0 }, function(response) {
+      if (response.meta.success) {
+        render({ target: 'publicize-bounty-button-container-'+bounty.id }, unpublicize_bounty_link_element(bounty));
+      }
+    });
+  });
+
+  define('unpublicize_bounty', function(bounty) {
+    BountySource.api('/user/bounties/'+bounty.id, 'PUT', { anonymous: 1 }, function(response) {
+      if (response.meta.success) {
+        render({ target: 'publicize-bounty-button-container-'+bounty.id }, publicize_bounty_link_element(bounty));
+      }
+    });
+  });
+
 
 }
