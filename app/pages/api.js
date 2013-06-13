da7ea66addece157d8c98623141771f1798aef64
Cory Boyd <corytheboyd@gmail.com>
2013-06-13 12:31:46 -0700

    fundraiser updates, pledges

--- app/pages/api.js
+++ app/pages/api.js
@@ -1,9 +1,12 @@
 angular.module('api.bountysource',[]).
   factory('$api', function($http, $q){
+    // var api_host = "https://api.bountysource.com";
+    var api_host = "http://api.bountysource.dev";
+
     return {
       fundraiser_cards: function() {
         var deferred = $q.defer();
-        $http.jsonp("https://api.bountysource.com/fundraisers/cards?callback=JSON_CALLBACK").success(function(response) {
+        $http.jsonp(api_host+"/fundraisers/cards?callback=JSON_CALLBACK").success(function(response) {
           deferred.resolve(response.data.in_progress.concat(response.data.completed));
         });
         return deferred.promise;
@@ -11,7 +14,15 @@ angular.module('api.bountysource',[]).
 
       fundraiser_get: function(id) {
         var deferred = $q.defer();
-        $http.jsonp("https://api.bountysource.com/user/fundraisers/"+id+"?callback=JSON_CALLBACK").success(function(response) {
+        $http.jsonp(api_host+"/user/fundraisers/"+id+"?callback=JSON_CALLBACK").success(function(response) {
+          deferred.resolve(response.data);
+        });
+        return deferred.promise;
+      },
+
+      fundraiser_pledges_get: function(id) {
+        var deferred = $q.defer();
+        $http.jsonp(api_host+"/user/fundraisers/"+id+"/pledges?callback=JSON_CALLBACK").success(function(response) {
           deferred.resolve(response.data);
         });
         return deferred.promise;
@@ -19,7 +30,7 @@ angular.module('api.bountysource',[]).
 
       people_recent: function(id) {
         var deferred = $q.defer();
-        $http.jsonp("https://api.bountysource.com/user/recent?callback=JSON_CALLBACK").success(function(response) {
+        $http.jsonp(api_host+"/user/recent?callback=JSON_CALLBACK").success(function(response) {
           deferred.resolve(response.data);
         });
         return deferred.promise;
@@ -27,7 +38,7 @@ angular.module('api.bountysource',[]).
 
       person_get: function(id) {
         var deferred = $q.defer();
-        $http.jsonp("https://api.bountysource.com/users/"+id+"?callback=JSON_CALLBACK").success(function(response) {
+        $http.jsonp(api_host+"/users/"+id+"?callback=JSON_CALLBACK").success(function(response) {
           console.log(response);
           deferred.resolve(response.data);
         });
@@ -36,7 +47,7 @@ angular.module('api.bountysource',[]).
 
       project_cards: function() {
         var deferred = $q.defer();
-        $http.jsonp("https://api.bountysource.com/trackers/cards?callback=JSON_CALLBACK").success(function(response) {
+        $http.jsonp(api_host+"/trackers/cards?callback=JSON_CALLBACK").success(function(response) {
           console.log(response);
           deferred.resolve(response.data.featured_trackers.concat(response.data.all_trackers));
         });
@@ -45,7 +56,7 @@ angular.module('api.bountysource',[]).
 
       tracker_get: function(id) {
         var deferred = $q.defer();
-        $http.jsonp("https://api.bountysource.com/trackers/"+id+"/overview?callback=JSON_CALLBACK").success(function(response) {
+        $http.jsonp(api_host+"/trackers/"+id+"/overview?callback=JSON_CALLBACK").success(function(response) {
           console.log(response);
           deferred.resolve(response.data);
         });
@@ -54,7 +65,7 @@ angular.module('api.bountysource',[]).
 
       issue_get: function(id) {
         var deferred = $q.defer();
-        $http.jsonp("https://api.bountysource.com/issues/"+id+"?callback=JSON_CALLBACK").success(function(response) {
+        $http.jsonp(api_host+"/issues/"+id+"?callback=JSON_CALLBACK").success(function(response) {
           console.log(response);
           deferred.resolve(response.data);
         });
