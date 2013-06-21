c7776069e84425f3e99b9089101e1346556306c4
Warren Konkel <wkonkel@gmail.com>
2013-06-20 18:48:31 -0700

    ditch My

--- app/pages/settings/tabs.js
+++ app/pages/settings/tabs.js
@@ -3,11 +3,9 @@
 angular.module('app')
   .controller('SettingsTabs', function($scope, $location) {
     $scope.tabs = [
-      { name: 'Public Profile', url: '/settings' },
-      { name: 'Login and Password', url: '/settings/auth' },
-      { name: 'Linked Accounts', url: '/settings/linked' },
-      { name: 'Email Settings', url: '/settings/email' },
-      { name: 'Project Plugins', url: '/settings/project' }
+      { name: 'Profile', url: '/settings' },
+      { name: 'Accounts', url: '/settings/accounts' },
+      { name: 'Email', url: '/settings/email' }
     ];
     $scope.is_active = function(url) {
       return url == $location.path();
