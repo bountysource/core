41ac97a3ad220de7e42e034c5947600ce355078b
Cory Boyd <corytheboyd@gmail.com>
2013-06-22 18:28:50 -0700

    rewards page

--- app/pages/fundraisers/controllers/rewards.js
+++ app/pages/fundraisers/controllers/rewards.js
@@ -11,10 +11,17 @@ angular.module('app')
 
   .controller('FundraiserRewardsInfoController', function ($scope, $routeParams, $location, $api) {
     $scope.fundraiser = $api.fundraiser_info_get($routeParams.id).then(function(response) {
-
       $scope.rewards = response.rewards;
-      console.log(response);
+
+      // initially open all of the tabs
+      angular.forEach($scope.rewards, function(r) { r.$is_open = true; });
 
       return response;
     });
+
+    $scope.expand_all = true;
+    $scope.toggle_expand_all = function() {
+      $scope.expand_all = !$scope.expand_all;
+      for (var i=0; i<$scope.rewards.length; i++) { $scope.rewards[i].$is_open = $scope.expand_all; }
+    };
   });
