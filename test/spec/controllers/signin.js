f6519a633d1b1277f8e8f77a405f0005a2fc9369
Daniel Nakashima <nakashima.dy@gmail.com>
2013-06-25 12:04:32 -0700

    get karma tests passing

--- test/spec/controllers/signin.js
+++ test/spec/controllers/signin.js
@@ -7,17 +7,19 @@ describe('Controller: Signin', function () {
   beforeEach(module('app'));
 
   var Signin,
+    api,
     scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $api) {
     scope = $rootScope.$new();
+    api = $api;
     Signin = $controller('Signin', {
       $scope: scope
     });
   }));
 
   it('should do something super sipmle', function () {
-    expect(scope.github_signin_url).toBe('https://api.bountysource.com/auth/github?redirect_url=http%3A%2F%2Flocalhost%3A9000%2F');
+    expect(api.signin_url_for('github')).toBe('https://api.bountysource.com/auth/github?redirect_url=http%3A%2F%2Flocalhost%3A9000%2Fsignin%2Fcallback%3Fprovider%3Dgithub');
   });
 });
