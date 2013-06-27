3e3ff964e46f4054ed11c245c94dc342e14de081
Warren Konkel <wkonkel@gmail.com>
2013-06-26 18:04:14 -0700

    change email, password, etc

--- app/pages/settings/accounts.js
+++ app/pages/settings/accounts.js
@@ -9,7 +9,7 @@ angular.module('app')
         resolve: $person
       });
   })
-  .controller('AccountSettings', function($scope, $api, $location) {
+  .controller('AccountSettings', function($scope, $api) {
     $scope.set_post_auth_url = function() {
       $api.set_post_auth_url($location.url());
     };
@@ -17,6 +17,24 @@ angular.module('app')
     $scope.github_link = $api.signin_url_for('github');
     $scope.twitter_link = $api.signin_url_for('twitter');
     $scope.facebook_link = $api.signin_url_for('facebook');
+
+    $scope.form_data = {};
+    $scope.change_password = function() {
+      $scope.error = $scope.success = null;
+
+      var req = {
+        current_password: $scope.form_data.current_password,
+        new_password: $scope.form_data.new_password,
+        password_confirmation: $scope.form_data.new_password
+      };
+      $api.change_password(req).then(function(response) {
+        if (response.error) {
+          $scope.error = response.error;
+        } else {
+          $scope.success = 'Successfully updated password!';
+        }
+      });
+    }
 
   });
 
--- app/pages/settings/email.js
+++ app/pages/settings/email.js
@@ -5,11 +5,28 @@ angular.module('app')
     $routeProvider
       .when('/settings/email', {
         templateUrl: 'pages/settings/email.html',
-        controller: 'Settings',
+        controller: 'SettingsEmail',
         resolve: $person
       });
   })
-  .controller('Settings', function($scope, $routeParams, $api) {
-    console.log('hax', $scope, $routeParams, $api);
+  .controller('SettingsEmail', function($scope, $routeParams, $api) {
+    $scope.form_data = {
+      email: $scope.current_person.email,
+      weekly_newsletter: !$scope.current_person.exclude_from_newsletter
+    }
+
+    $scope.submit = function() {
+      $scope.error = $scope.success = null;
+
+      var updates = { email: $scope.form_data.email, exclude_from_newsletter: !$scope.form_data.weekly_newsletter };
+      $api.person_put(updates).then(function(response) {
+        if ($scope.current_person.email == $scope.form_data.email) {
+          $scope.success = 'Email settings updated!';
+        } else {
+          $scope.error = 'Unable to update email settings!';
+        }
+      });
+    };
+
   });
 
