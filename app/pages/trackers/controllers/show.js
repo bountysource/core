68e63fb1ea5b7cb12fba082345ed421b2cc67c29
Cory Boyd <corytheboyd@gmail.com>
2013-06-28 18:04:24 -0700

    update tracker show a bit

---
+++ app/pages/trackers/controllers/show.js
@@ -0,0 +1,54 @@
+'use strict';
+
+angular.module('app')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/trackers/:id', {
+        templateUrl: 'pages/trackers/show.html',
+        controller: 'TrackerShow'
+      });
+  })
+  .controller('TrackerShow', function ($scope, $routeParams, $api) {
+    $api.tracker_get($routeParams.id).then(function(tracker) {
+      console.log(tracker);
+
+      $scope.tracker = tracker;
+
+      // merge all of the issue arrays into one
+      tracker.issues = [];
+      for (var i in tracker.issues_valuable) { tracker.issues.push(tracker.issues_valuable[i]); }
+      for (var i in tracker.issues_popular) { tracker.issues.push(tracker.issues_popular[i]); }
+      for (var i in tracker.issues_newest) { tracker.issues.push(tracker.issues_newest[i]); }
+
+      // turn all of the bounty totals into floats. dunno why that isn't the case.
+      for (var i in tracker.issues) { tracker.issues[i].bounty_total = parseFloat(tracker.issues[i].bounty_total); }
+    });
+
+    $scope.issue_filter_options = {
+      bounty_min: null,
+      bounty_max: null,
+      only_valuable: false
+    };
+
+    $scope.update_filter_options = function() {
+      $scope.issue_filter_options = {
+        bounty_min: parseFloat($scope.issue_filter_options.bounty_min),
+        bounty_max: parseFloat($scope.issue_filter_options.bounty_max)
+      };
+    };
+    // $scope.update_filter_options();
+
+    $scope.issue_filter = function(issue) {
+      var bounty_total = parseFloat(issue.bounty_total);
+      var bounty_min = parseFloat($scope.issue_filter_options.bounty_min);
+      var bounty_max = parseFloat($scope.issue_filter_options.bounty_max);
+
+      if (!isNaN(bounty_min) && bounty_total < bounty_min) return false;
+      if (!isNaN(bounty_max) && bounty_total > bounty_max) return false;
+      if ($scope.issue_filter_options.only_valuable && bounty_total <= 0) return false;
+      return true;
+    };
+
+    $scope.issue_order_columns = [];
+  });
+
