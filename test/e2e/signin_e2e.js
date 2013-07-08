83ea834c6028b849601319ba5e2863aacec301b2
Daniel Nakashima <nakashima.dy@gmail.com>
2013-07-08 14:36:18 -0700

    update to tests siginin still incomplete

--- test/e2e/signin_e2e.js
+++ test/e2e/signin_e2e.js
@@ -4,75 +4,95 @@
 var fill_form =  function(data) {
 	for(var key in data) {
 		var id = 'form_data.' + key;
-		input(id).enter(data[key]);
+		if (typeof(data[key])==="boolean") {input(id).check();} else {input(id).enter(data[key]);}
 	}
 };
+var MOCK = {
+	signed_in_user : {
+		email: "mr_manly@gmail.com",
+		password: "MANLINESS123456"
+	},
+	new_user_valid : {
+		email: "mr_manly@gmail.com",
+		password: "MANLINESS123456",
+		first_name: "Mister",
+		last_name: "ManlyMan",
+		display_name: "TheManliest",
+		terms: true
+	}
+};
+
 describe('Scenario: Signining In --', function() {
-	var MOCK = {
-		signed_in_user : {
-			email: "dandan@gmail.com",
-			password: "dandan123456"
-		}
-	};
-	var new_user_valid = {email: "mrmanly@gmail.com", password: "MANLINESS123456", first_name: "Mister", last_name: "ManlyMan", display_name: "TheManliest", terms: true };
-	// MOCK.signed_in_user = signed_in_user;
-	MOCK.new_user_valid = new_user_valid;
-
-  beforeEach(function () {
-    browser().navigateTo("/signin");
-  });
-  /////////////////////////////////////////////////////////////////
-  //////////////////////// EXAMPLE TEST ///////////////////////////
-  /////////////////////////////////////////////////////////////////
-  // it('should auto compile', function () {
-		// expect(element('h2').text()).toBe('Please sign in to continue');
-  // });
-	/////////////////////////////////////////////////////////////////
-
-  describe('INITIAL STATE', function () {
-		it("EMAIL field should be initialized to example email", function() {
+	beforeEach(function () {
+		// element("a:contains('staging')").click();
+		browser().navigateTo("/signin");
+	});
+	describe('INITIAL STATE', function () {
+		it("EMAIL field should be initialized", function() {
 			expect(element('#inputEmail').attr('placeholder')).toBe('john@doe.com');
+			// expect(element('#inputEmail').attr('placeholder')).toMatch('+[a-zA-Z]@*.3[a-zA-Z]');
+			expect(element('#inputEmail:visible').count()).toBe(1);
 		});
-		it("PASSWORD field should be initialized to example password", function() {
-			expect(element('#inputPassword').attr('placeholder')).toBe('abcd1234');
+		it("PASSWORD field should initialized", function() {
+			// expect(element('#inputPassword').attr('placeholder')).toBe('abcd1234');
+			expect(element('#inputPassword').attr('placeholder')).toMatch('[a-zA-Z].*[0-9]|[0-9].*[a-zA-Z]');
+			expect(element('#inputPassword:visible').count()).toBe(1);
 		});
-		describe('HIDDEN values', function() {
-			it('should have FIRST NAME hidden', function() {
-				expect(element('input:visible')).not().toBeNull();
-			});
+		it("should HIDE REST of FIELDS", function() {
+			expect(element('#inputDisplayName:hidden').count()).toBe(1);
+			expect(element('#inputLastName:hidden').count()).toBe(1);
+			expect(element('#inputFirstName:hidden').count()).toBe(1);
 		});
 	});
 	describe("INITIAL SUBMISSION", function() {
-	  it("should allow you to SIGIN_IN", function() {
+		it("should allow you to SIGIN_IN", function() {
 			input('form_data.email').enter(MOCK.new_user_valid.email);
-	    input('form_data.password').enter('aa123');
-		  element("button:contains('Sign In')").click();
-	    expect(element(".help-inline:visible").text()).toBe("Available!");
-	  });
-	  it("should ERROR for incorrectly formatted PASSWORD", function() {
-	    // input('form_data.email').enter(MOCK.new_user_valid.email);
-	    input('form_data.password').enter('aaaaaaaaaaaaaaaaaaaaa');
-	    element("button:contains('Sign In')").click();
-	    input('form_data.first_name').enter(MOCK.new_user_valid.first_name);
-	    input('form_data.last_name').enter(MOCK.new_user_valid.last_name);
-	    input('form_data.display_name').enter(MOCK.new_user_valid.last_name);
-	    input('form_data.terms').check();
-	    element("button:contains('Sign Up')").click();
-	    // console.log(element('.alert.alert-error').text());
-	    expect(element('.alert.alert-error').text()).toContain('Password must contain a letter and a number');
-	  });
+			input('form_data.password').enter('aa123');
+			expect(element(".help-inline").text()).toContain("Available!");		//INCOMPLETE
+			// expect(element("ng-show=='signup'").text()).toContain("Available!");
+		});
+		it("should FIND user that EXISTS", function() {
+			input('form_data.email').enter(MOCK.new_user_valid.email);
+			expect(element(".help-inline").text()).toContain("Found!");		//INCOMPLETE
+			// expect(binding('signin_or_signup')).toBe("signin");
+		});
+		it("should ERROR for incorrectly formatted PASSWORD", function() {
+			var no_number_password_user = {};
+			for (var key in MOCK.signed_in_user) {no_number_password_user[key] = MOCK.signed_in_user[key];}
+			no_number_password_user.password = 'aaaaaaaaaaaaaaaaaaaaa';
+			fill_form(no_number_password_user);
+			element("button:contains('Sign In')").click();
+			expect(element('.alert.alert-error').text()).toContain('Password not correct');
+			// expect(binding('form.input.$valid')).toEqual('false');
+		});
+	});
+	describe("VALID SIGN UP", function() {
+		it("should NOT show errors for for correct signup", function() {
+			fill_form(MOCK.new_user_valid);
+			element("button:contains('Sign Up')").click();		//INCOMPLETE
+			// input('form_data.password').enter(MOCK.new_user_valid.password);
+			// fill_form(MOCK.signed_in_user);
+			// expect(element('.alert.alert-error').text()).toBeNull();
+		});
+	});
 
-	  it("should ERROR for incorrectly formatted PASSWORD", function() {
-	    // input('form_data.email').enter(MOCK.new_user_valid.email);
-	    // input('form_data.password').enter('aa123');
-	    fill_form(MOCK.signed_in_user);
-	    element("button:contains('Sign In')").click();
-	    input('form_data.first_name').enter(MOCK.new_user_valid.first_name);
-	    input('form_data.last_name').enter(MOCK.new_user_valid.last_name);
-	    input('form_data.display_name').enter(MOCK.new_user_valid.last_name);
-	    input('form_data.terms').check();
-	    element("button:contains('Sign Up')").click();
-	    expect(element('.alert.alert-error').text()).toContain('Password is too short (minimum is 8 characters)');
-	  });
+	// since user already gets created look at the possible paths
+	describe("REDIRECT to the HOMEPAGE", function() {
+		it("should have HOME URL", function() {
+			fill_form(MOCK.signed_in_user);
+			element("button:contains('Sign In')").click();
+			expect(browser().location().url()).toBe("/");
+		});
+		it("should show user's DISPLAY_NAME in NAVBAR", function() {
+			expect(binding("current_person.display_name")).toBe(MOCK.new_user_valid.display_name);
+		});
 	});
 });
+/////////////////////////////////////////////////////////////////
+//////////////////////// EXAMPLE TEST ///////////////////////////
+/////////////////////////////////////////////////////////////////
+// it('should auto compile', function () {
+	// expect(element('h2').text()).toBe('Please sign in to continue');
+// });
+/////////////////////////////////////////////////////////////////
+
--- test/spec/controllers/issue_spec.js
+++ test/spec/controllers/issue_spec.js
@@ -1,76 +1,50 @@
-/* jshint -W117 */
-'use strict';
+// /* jshint -W117 */
+// 'use strict';
 
-describe('Controller: Signin', function () {
+// describe('Controller: Signin', function () {
 
-  //var signin;
-  var api;
-  var scope;
-  var routeparams;
-  var httpMock;
-  var createController;
-  // var ctrl_service;
+//   //var signin;
+//   var api;
+//   var scope;
+//   var routeparams;
+//   var httpMock;
+//   var createController;
 
-  var MOCKS = {
-    forgot_password: {
-      message: "ello ello",
-      email_is_registered: true,
-      error: "some error"
-    },
-    email_registered: {
-      message: "ello ello",
-      email_is_registered: true,
-      error: "some error"
-    },
-    email_not_registered: {
-      message: "ello ello",
-      email_is_registered: false,
-      error: "some error"
-    },
-    form_data: {
-      email: "dan@dan.com",
-      password: "dandan123",
-      first_name: "dan_first",
-      last_name: "dan_last",
-      display_name: "dandan",
-      terms: true
-    }
-  };
-//  var HELPERS = {
-//    promise: function (mock_data) {
-//      return { then: function (fn) {
-//        return fn(mock_data);
-//      } };
-//    },
-//    createController: function (ctrl_id) {
-//      return $controller(ctrl_id, { $scope: $rootScope });
-//    }
-//  };
+//   var MOCKS = {
+//     forgot_password: {
+//       message: "ello ello",
+//       email_is_registered: true,
+//       error: "some error"
+//     },
+//     email_registered: {
+//       message: "ello ello",
+//       email_is_registered: true,
+//       error: "some error"
+//     },
+//     email_not_registered: {
+//       message: "ello ello",
+//       email_is_registered: false,
+//       error: "some error"
+//     },
+//     form_data: {
+//       email: "dan@dan.com",
+//       password: "dandan123",
+//       first_name: "dan_first",
+//       last_name: "dan_last",
+//       display_name: "dandan",
+//       terms: true
+//     }
+//   };
 
-  // Initialize the controller and a mock scope
-  beforeEach(module('app'));
+//   beforeEach(module('app'));
 
-  beforeEach(inject(function ($controller, $rootScope, $routeParams, $api, $httpBackend) {
-    scope = $rootScope.$new();
-    api = $api;
-    routeparams = $routeParams;
-    httpMock = $httpBackend;
-    // signin = $controller('Signin', {$scope: scope});
-    // ctrl_service = $controller;
-    createController = function () {
-      return $controller('Signin', {'$scope': $rootScope });
-    };
-  }));
+//   beforeEach(inject(function ($controller, $rootScope, $routeParams, $api, $httpBackend) {
+//     scope = $rootScope.$new();
+//     api = $api;
+//     routeparams = $routeParams;
+//     httpMock = $httpBackend;
+//   }));
 
-  it('should let me use httpBackend', function () {
-    httpMock.expectJSONP('/signin').respond(function (method, url, data, headers) {
-      console.log('Received these data:', method, url, data, headers);
-    });
-    //var ctrl = createController();
-    scope.form_data = MOCKS.form_data;
-//    scope.signin();
-//    expect(scope.error).toEqual(MOCKS.email_not_registered.error);
-//    httpMock.flush();
-  });
+//   });
 
-});
+// });
