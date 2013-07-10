148951bc9b858e2462a16c94cea7af815fc2234d
Cory Boyd <corytheboyd@gmail.com>
2013-07-10 11:36:05 -0700

    save route when signing in with linked accounts

--- app/pages/layout/navbar.js
+++ app/pages/layout/navbar.js
@@ -3,5 +3,10 @@
 angular.module('app')
   .controller('Navbar', function ($scope, $api) {
     $scope.setEnv = $api.setEnvironment;
+  })
+  .controller('NavbarLinkedAccountSignin', function($scope, $location, $api) {
+    $scope.save_route = function() {
+      $api.set_post_auth_url($location.url());
+    };
   });
 
