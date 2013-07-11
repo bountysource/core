6f241d24888aa43206f457e67a53b1d359660ed4
Cory Boyd <corytheboyd@gmail.com>
2013-07-11 12:48:49 -0700

    handle undefined

--- app/pages/filters.js
+++ app/pages/filters.js
@@ -24,6 +24,7 @@ angular.module('app').
     };
   }).filter('slice', function() {
     return function(a,start,end) {
+      if (!a) return [];
       return a.slice(start,end);
     };
   }).filter('truncate', function() {
