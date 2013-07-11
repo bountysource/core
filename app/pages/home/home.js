3b06cc449531e39caeccd5f6f60d7239104bd0ee
Cory Boyd <corytheboyd@gmail.com>
2013-07-10 18:39:47 -0700

    home page love

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -11,12 +11,8 @@ angular.module('app')
   .controller('HomeCtrl', function ($scope, $window, $api, $twttr) {
     $scope.fundraisers = $api.fundraiser_cards();
 
-    $scope.show_developers_type = 'top';
-    $scope.show_backers_type = 'top';
-
     // $scope.recent_people = $api.people_recent();
     $api.people_interesting().then(function(people) {
-      console.log(people);
       $scope.people = people;
     });
 
