9bc6f3808b2ceb658b8ca1d50448cb9d67e051d1
Cory Boyd <corytheboyd@gmail.com>
2013-07-11 15:10:17 -0700

    fees and learn pages. link to learn from homepage

---
+++ app/pages/about/learn.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('app')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/learn', {
+        templateUrl: 'pages/about/learn.html',
+        controller: 'Static'
+      });
+  });
