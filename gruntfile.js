de598dec7dad215ce161c20ad0e5d25ba141cece
Warren Konkel <wkonkel@gmail.com>
2013-07-11 10:46:43 -0700

    more deployment stuff

--- gruntfile.js
+++ gruntfile.js
@@ -8,6 +8,13 @@ module.exports = function (grunt) {
   // load all grunt tasks
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
+  var cloudfile_config;
+  try {
+    cloudfile_config = grunt.file.readJSON('cloudfiles.json');
+  } catch (e) {
+    cloudfile_config = grunt.file.readJSON('cloudfiles.example.json');
+  };
+
   grunt.initConfig({
     watch: {
       options: {
@@ -89,6 +96,7 @@ module.exports = function (grunt) {
           ]
         }]
       },
+      dist_assets: ['dist/assets', 'dist/compiled'],
       server: '.tmp',
       templates: ['dist/templates.js', 'dist/pages']
     },
@@ -162,7 +170,7 @@ module.exports = function (grunt) {
       }
     },
     usemin: {
-      html: ['dist/**/*.html'],
+      html: ['dist/*.html'],
       css: ['dist/assets/**/*.css'],
       options: {
         dirs: ['dist']
@@ -173,7 +181,12 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: 'app/images',
-          src: '**/*.{png,jpg,jpeg}',
+          src: '*.{png,jpg,jpeg,gif}',
+          dest: 'dist/assets'
+        },{
+          expand: true,
+          cwd: 'app/components/bootstrap/docs/assets/img',
+          src: 'glyph*.png',
           dest: 'dist/assets'
         }]
       }
@@ -187,7 +200,7 @@ module.exports = function (grunt) {
           dest: 'dist'
         }]
       },
-      pages: {
+      templates: {
         options: {
           // NOTE: this saves 20kb or so but we rely on whitespace in some places to add horizontal spacing between elements
           //collapseWhitespace: true
@@ -220,15 +233,6 @@ module.exports = function (grunt) {
         }]
       }
     },
-    rev: {
-      dist: {
-        files: {
-          src: [
-            'dist/assets/*'
-          ]
-        }
-      }
-    },
     copy: {
       dist: {
         files: [{
@@ -240,6 +244,55 @@ module.exports = function (grunt) {
             'favicon.ico',
             'robots.txt'
           ]
+        }]
+      }
+    },
+    md5: {
+      options: {
+        encoding: null,
+        keepBasename: false,
+        keepExtension: true,
+        afterEach: function(changes) {
+          var hash_map = grunt.config('md5_hash') || {};
+          hash_map[changes.oldPath.split('/').pop()] = changes.newPath.split('/').pop();
+          grunt.config('md5_hash', hash_map);
+        }
+      },
+
+      binary: {
+        files: {
+          'dist/compiled/': ['dist/assets/*.{png,jpg,jpeg,gif,woff}']
+        }
+      },
+
+      css_js: {
+        files: {
+          'dist/compiled/': ['dist/assets/*.{css,js}']
+        }
+      }
+    },
+    md5cdn: {
+      options: {
+        base_url: cloudfile_config.base_url
+      },
+      css: {
+        src: 'dist/assets/*.css'
+      },
+      js: {
+        src: 'dist/assets/*.js'
+      },
+      html: {
+        src: 'dist/*.html'
+      }
+    },
+    cloudfiles: {
+      prod: {
+        'user': cloudfile_config.user,
+        'key': cloudfile_config.key,
+        'upload': [{
+          'container': cloudfile_config.container,
+          'src': 'dist/compiled/*',
+          stripcomponents: 2
         }]
       }
     }
@@ -305,20 +358,27 @@ module.exports = function (grunt) {
   ]);
 
   grunt.registerTask('build', [
-    //'test',
-    'clean:dist',
-    'useminPrepare',
-    'imagemin',
-    'cssmin',
-    'htmlmin',
-    'ngtemplates',
-    'concat',
-    'clean:templates',
-    'copy',
-    'ngmin',
-    'uglify',
-    'rev',
-    'usemin'
+    //'test',              // run tests first, don't build unless all tests pass
+    'clean:dist',        // start off with empty folder
+    'copy',              // copy in static files like favicon.ico and robots.txt
+    'imagemin',          // copy in minified version of images
+    'md5:binary',        // create md5-named copies of binary files (images, fonts, etc)
+    'htmlmin:index',     // copy index.html, 404.html, etc
+    'useminPrepare',     // update in-memory configs for concat/cssmin/uglify based on "build blocks"
+    'usemin',            // replace "build blocks" in html files with path to "compiled" version
+    'htmlmin:templates', // copy in angular templates
+    'ngtemplates',       // compile angular templates into templates.js and add to list of files to concat
+    'concat',            // concat css/js files from usemin and angular templates
+    'clean:templates',   // remove angular templates as they're now compiled and included in JS
+    'ngmin',             // tweak JS files so uglify works with angular dependencies. specifically: function($scope)... becomes ['$scope', function(a)...]
+    'uglify',            // compress JS files
+    'cssmin',            // compress CSS files
+    'md5cdn:js',         // update angular templates with CDN urls to md5:binary files
+    'md5cdn:css',        // update compiled css with CDN urls
+    'md5:css_js',        // create md5 copies of css/js in dist/compiled
+    'md5cdn:html',       // update root HTML with CDN'd css/js
+    'cloudfiles'        // copy all of compiled/ up to CDN
+    //'clean:dist_assets'  // clean up assets now that they're all up on CDN
   ]);
 
   grunt.registerTask('default', ['build']);
@@ -347,5 +407,49 @@ module.exports = function (grunt) {
 
     });
   });
+
+
+  grunt.registerMultiTask('md5cdn', 'Replace relative links with absolute CDN md5 urls', function() {
+    var base_url = this.options().base_url;
+
+    this.files.forEach(function(f) {
+      f.src.forEach(function(filepath) {
+        console.log("Processing", filepath);
+        var contents = grunt.file.read(filepath);
+
+        var md5_hash = grunt.config('md5_hash');
+        for (var k in md5_hash) {
+          var matcher = new RegExp('([\'"(])[./a-z_]*'+k+'([\'")])', 'g');
+          var cdn_url = base_url + md5_hash[k];
+          contents = contents.replace(matcher, '$1'+cdn_url+'$2');
+        }
+        grunt.file.write(filepath, contents);
+      })
+    });
+
+//    this.files.forEach(function(f) {
+//      var orig_src = grunt.file.read(f.dest);
+//      var src = ''+orig_src;
+//      var relative_path = f.dest.replace(/[^/]*?$/,'');
+//
+//      // add our own script references
+//      var script_tags = "<!-- html_src start -->\n";
+//      f.src.map(function(filepath) {
+//        script_tags += "    <script src=\"" + filepath.replace(relative_path,"") + "\" type=\"text\/javascript\"></script>\n";
+//      });
+//      script_tags += "    <!-- html_src end -->";
+//
+//      // remove existing tags
+//      src = src.replace(/<!-- html_src start -->[\s\S]+?<!-- html_src end -->/,script_tags);
+//
+//      if (src !== orig_src) {
+//        grunt.log.writeln('File "' + f.dest + '" updated.');
+//        grunt.file.write(f.dest, src);
+//      }
+//
+//    });
+  });
+
+
 
 };
