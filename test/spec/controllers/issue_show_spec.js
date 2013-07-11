f8a6fc0c5fb381c4d29ac1f965de15d143159953
Daniel Nakashima <nakashima.dy@gmail.com>
2013-07-10 17:08:46 -0700

    expanded on the issues_show and tracker_show units

--- test/spec/controllers/issue_show_spec.js
+++ test/spec/controllers/issue_show_spec.js
@@ -1,7 +1,99 @@
 /* jshint -W117 */
 'use strict';
 
-describe("Issue Show Controller -- ", function() {
-  it("should be cool", function() {
-  });
+describe("TrackerShow Controller --", function() {
+	//////////////////////////////
+	////////// SETUP /////////////
+	//////////////////////////////
+
+	var PROMISE = {
+		reply_with: function (mock_return) {
+			return { then: function (handler) {return handler===undefined ? handler() : handler(mock_return);} };
+		}
+	};
+
+	var MOCKS = {
+		tracker: {
+		  issues_valuable: jasmine.any(Array),
+		  issues_popular: jasmine.any(Array),
+		  issues_newest:  jasmine.any(Array),
+		  followed: true
+		},
+		routeParams: {
+		  amount: 0,
+		  anonymous: null,
+		  payment_mehtod: null
+		},
+		response: {
+			data: {
+				id: jasmine.any(Number)
+			}
+		}
+	};
+	/////////////////////////////////////
+	/////////////// TESTS  //////////////
+	/////////////////////////////////////
+	var issue_ctrl;
+	var api;
+	var scope;
+	var routeParams;
+	var payment;
+	var location;
+	var window;
+
+	beforeEach(module('app'));
+
+	beforeEach(inject(function ($controller, $rootScope, $routeParams, $api, $payment, $window, $location) {
+		scope = $rootScope.$new();
+
+		api = $api;
+		spyOn(api, 'issue_get').andReturn(MOCKS.response);	//just return the data ##### NO PROMISE FOR THIS CALL
+		spyOn(api, 'set_post_auth_url').andReturn(PROMISE.reply_with());
+
+		routeParams = $routeParams;
+		routeParams = MOCKS.routeParams;
+
+		payment = $payment;
+
+		window = $window;
+
+		location = $location;
+
+		// only construct controller when api mock setup
+		issue_ctrl = $controller('IssueShow', {$scope: scope, $routeParams: routeParams, $api: api, $payment: payment});
+	}));
+
+	describe("INIT:", function() {
+		describe("bounty:", function() {
+		  it("should initialize bounty", function() {expect(scope.bounty).toBeDefined(); });
+		  it("should Default for 'amount' ", function() {expect(scope.bounty.amount).toEqual(0); });
+		  it("should Default for 'anonymous'", function() {expect(scope.bounty.anonymous).toBeFalsy(); });
+
+		  xit("initialize for 'anonymous'", inject(function($scope, $routeParams, $controller) {
+			  // var scope;
+			  var routeParams;
+			  // scope = $rootScope.$new();
+			  routeParams = MOCKS.routeParams;
+			  routeParams.anonymous = true;
+				issue_ctrl = $controller('IssueShow', {$scope: scope, $routeParams: routeParams});
+				expect(scope.bounty.anonymous).toBeTruthy();
+			}));
+
+		});
+		// issue is an object that gets returned from api
+		it("should make an 'issue_get' call to API", function() {
+		  expect(api.issue_get).toHaveBeenCalled();
+		  expect(api.issue_get).toHaveBeenCalledWith(routeParams.id, jasmine.any(Function));
+		});
+		it("should assign response Issue to issue in scope", function() {
+			expect(scope.issue).toEqual(MOCKS.response);
+		});
+
+		// cant mock out too much interdependency can;t access inner function
+		xit("should have a create_payment function", function() {
+			var issue_id = jasmine.any(Number);
+			scope.issue(issue_id, jasmine.any(Function));
+		  expect(scope.create_payment).toBeDefined();
+		});
+	});
 });
--- test/spec/controllers/tracker_show_spec.js
+++ test/spec/controllers/tracker_show_spec.js
@@ -2,7 +2,9 @@
 'use strict';
 
 describe("TrackerShow Controller --", function() {
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////
+////////// SETUP /////////////
+//////////////////////////////
   //reply_with takes in mock data and returns an
   // OBJECT  that has a then attr is a
   // FUNCTION that takes in a handler that will have any number of args
@@ -12,67 +14,61 @@ describe("TrackerShow Controller --", function() {
       return { then: function (handler) {return handler===undefined ? handler() : handler(mock_return);} };
     }
   };
+  
   function get_new_object(mock) {
     var new_obj = {};
     for(var key in mock){ new_obj[key] = mock[key]; }
     return new_obj;
   }
-  // this.addMatchers({
-  //   toBeLessThan: function(expected) {
-  //     var actual = this.actual;
-  //     var notText = this.isNot ? " not" : "";
 
-  //     this.message = function () {
-  //       return "Expected " + actual + notText + " to be less than " + expected;
-  //     }
-  //     return actual < expected;
-  //   }
-  // });
-
-  var mock_issue_1 = {};
-  var mock_issue_2 = {};
-  var mock_tracker = {
-    issues_valuable: [mock_issue_1, mock_issue_2],
-    issues_popular: [mock_issue_1],
-    issues_newest: [mock_issue_2],
-    followed: true
+  var MOCKS = {
+    issue_1: {},
+    issue_2: {},
+    tracker: {
+      issues_valuable: jasmine.any(Array),
+      issues_popular: jasmine.any(Array),
+      issues_newest:  jasmine.any(Array),
+      followed: true
+    },
+    route_param: {
+      id:jasmine.any(Number)
+    },
+    tag: {
+      name: null
+    }
   };
-  var mock_route_param = {id:5};
-  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+  /////////////////////////////////////
+  /////////////// TESTS  //////////////
+  /////////////////////////////////////
   var tracker_ctrl;
   var api;
   var scope;
   var routeparams;
-  var httpBackend;
 
   beforeEach(module('app'));
 
-  beforeEach(inject(function ($controller, $rootScope, $routeParams, $api, $httpBackend) {
+  beforeEach(inject(function ($controller, $rootScope, $routeParams, $api) {
     scope = $rootScope.$new();
 
     api = $api;
-
-    spyOn(api, 'tracker_get').andReturn(PROMISE.reply_with(mock_tracker));
+    spyOn(api, 'tracker_get').andReturn(PROMISE.reply_with(MOCKS.tracker));
     spyOn(api, 'tracker_unfollow').andReturn(PROMISE.reply_with());
     spyOn(api, 'tracker_follow').andReturn(PROMISE.reply_with());
 
     routeparams = $routeParams;
-    routeparams.id = mock_route_param.id;
+    routeparams.id = MOCKS.route_param.id;
 
-    httpBackend = $httpBackend;
-    // httpBackend.whenJSONP('/*/').respond({hello: "something"});
-
-    // only call controller when api mock setup
+    // only construct controller when api mock setup
     tracker_ctrl = $controller('TrackerShow', {$scope: scope, $routeParams: routeparams, $api: api});
   }));
+
   describe("INIT:", function() {
     it("should get TRACKERS from API", function() {
       expect(api.tracker_get).toHaveBeenCalled();
-      expect(api.tracker_get).toHaveBeenCalledWith(mock_route_param.id);
+      expect(api.tracker_get).toHaveBeenCalledWith(MOCKS.route_param.id);
     });
 
-    describe("Filter Options:", function() {
+    describe("Filter Options::", function() {
       it("should initialize issue_filter_options", function() {
         expect(scope.issue_filter_options).toBeDefined();
         expect(scope.issue_filter_options).not.toBeNull();
@@ -91,16 +87,18 @@ describe("TrackerShow Controller --", function() {
       });
     });
 
-    it("should be in reverse order", function() {
-      expect(scope.order_reverse).toBeTruthy();
-    });
-    it("should be ordered by Bounty Total", function() {
-      expect(scope.order_col).toEqual("bounty_total");
+    it("should set 'order_reverse' to true",        function() {expect(scope.order_reverse).toBeTruthy();         });
+    it("should set 'order_col' to 'bounty_total'",  function() {expect(scope.order_col).toEqual("bounty_total");  });
+
+    xdescribe("Ctrl Locals::", function() {
+      it("should set 'new_tag'", function() {
+        expect(tracker_ctrl.tracker).toBeDefined();
+      });
     });
 
   });
   xdescribe("AFTER tracker recieved", function() {
-    xit("should respond when NOT Following", function() {
+    it("should respond when NOT Following", function() {
       var unfollowed_tracker = get_new_object(mock_tracker);
       expect(unfollowed_tracker).not.toBeNull();
     });
