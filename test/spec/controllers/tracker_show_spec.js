493ad24bcc8b8b14d1cf98daeddbf6285d62422d
dynakashima <nakashima.dy@gmail.com>
2013-07-13 20:17:24 -0700

    update to repo updated tests jshint, e2e, unit passing

--- test/spec/controllers/tracker_show_spec.js
+++ test/spec/controllers/tracker_show_spec.js
@@ -2,105 +2,98 @@
 'use strict';
 
 describe("TrackerShow Controller --", function() {
-//////////////////////////////
-////////// SETUP /////////////
-//////////////////////////////
-  //reply_with takes in mock data and returns an
-  // OBJECT  that has a then attr is a
-  // FUNCTION that takes in a handler that will have any number of args
+  //////////////////////////////
+  ////////// SETUP /////////////
+  //////////////////////////////
 
   var PROMISE = {
     reply_with: function (mock_return) {
       return { then: function (handler) {return handler===undefined ? handler() : handler(mock_return);} };
     }
   };
-  
-  function get_new_object(mock) {
-    var new_obj = {};
-    for(var key in mock){ new_obj[key] = mock[key]; }
-    return new_obj;
-  }
 
   var MOCKS = {
-    issue_1: {},
-    issue_2: {},
     tracker: {
       issues_valuable: jasmine.any(Array),
       issues_popular: jasmine.any(Array),
       issues_newest:  jasmine.any(Array),
       followed: true
     },
-    route_param: {
-      id:jasmine.any(Number)
+    routeParams: {
+      amount: 0,
+      anonymous: null,
+      payment_mehtod: null
     },
-    tag: {
-      name: null
+    response: {
+      data: {
+        id: jasmine.any(Number)
+      }
     }
   };
   /////////////////////////////////////
   /////////////// TESTS  //////////////
   /////////////////////////////////////
-  var tracker_ctrl;
+  var issue_ctrl;
   var api;
   var scope;
-  var routeparams;
+  var routeParams;
+  var payment;
+  var location;
+  var window;
 
   beforeEach(module('app'));
 
-  beforeEach(inject(function ($controller, $rootScope, $routeParams, $api) {
+  beforeEach(inject(function ($controller, $rootScope, $routeParams, $api, $payment, $window, $location) {
     scope = $rootScope.$new();
 
     api = $api;
-    spyOn(api, 'tracker_get').andReturn(PROMISE.reply_with(MOCKS.tracker));
-    spyOn(api, 'tracker_unfollow').andReturn(PROMISE.reply_with());
-    spyOn(api, 'tracker_follow').andReturn(PROMISE.reply_with());
+    spyOn(api, 'issue_get').andReturn(MOCKS.response);  //just return the data ##### NO PROMISE FOR THIS CALL
+    spyOn(api, 'set_post_auth_url').andReturn(PROMISE.reply_with());
 
-    routeparams = $routeParams;
-    routeparams.id = MOCKS.route_param.id;
+    routeParams = $routeParams;
+    routeParams = MOCKS.routeParams;
+
+    payment = $payment;
+
+    window = $window;
+
+    location = $location;
 
     // only construct controller when api mock setup
-    tracker_ctrl = $controller('TrackerShow', {$scope: scope, $routeParams: routeparams, $api: api});
+    issue_ctrl = $controller('IssueShow', {$scope: scope, $routeParams: routeParams, $api: api, $payment: payment});
   }));
 
   describe("INIT:", function() {
-    it("should get TRACKERS from API", function() {
-      expect(api.tracker_get).toHaveBeenCalled();
-      expect(api.tracker_get).toHaveBeenCalledWith(MOCKS.route_param.id);
+    describe("bounty:", function() {
+      it("should initialize bounty", function() {expect(scope.bounty).toBeDefined(); });
+      it("should Default for 'amount' ", function() {expect(scope.bounty.amount).toEqual(0); });
+      it("should Default for 'anonymous'", function() {expect(scope.bounty.anonymous).toBeFalsy(); });
+
+      xit("initialize for 'anonymous'", inject(function($scope, $routeParams, $controller) {
+        // var scope;
+        var routeParams;
+        // scope = $rootScope.$new();
+        routeParams = MOCKS.routeParams;
+        routeParams.anonymous = true;
+        issue_ctrl = $controller('IssueShow', {$scope: scope, $routeParams: routeParams});
+        expect(scope.bounty.anonymous).toBeTruthy();
+      }));
+
+    });
+    // issue is an object that gets returned from api
+    it("should make an 'issue_get' call to API", function() {
+      expect(api.issue_get).toHaveBeenCalled();
+      expect(api.issue_get).toHaveBeenCalledWith(routeParams.id, jasmine.any(Function));
+    });
+    it("should assign response Issue to issue in scope", function() {
+      expect(scope.issue).toEqual(MOCKS.response);
     });
 
-    describe("Filter Options::", function() {
-      it("should initialize issue_filter_options", function() {
-        expect(scope.issue_filter_options).toBeDefined();
-        expect(scope.issue_filter_options).not.toBeNull();
-      });
-      describe("Initializing FilterOptionValues:", function() {
-        var options;
-        beforeEach(function() {
-          options = scope.issue_filter_options;
-        });
-        it("should set 'text'         to null",   function() {expect(options.text).toBeNull();            });
-        it("should set 'bounty_min'   to null",   function() {expect(options.bounty_min).toBeNull();      });
-        it("should set 'bounty_max'   to null",   function() {expect(options.bounty_max).toBeNull();      });
-        it("should set 'only_valuable'to false",  function() {expect(options.only_valuable).toBeFalsy();  });
-        it("should set 'hide_closed'  to false",  function() {expect(options.hide_closed).toBeFalsy();    });
-        it("should set 'hide_open'    to false",  function() {expect(options.hide_open).toBeFalsy();      });
-      });
-    });
-
-    it("should set 'order_reverse' to true",        function() {expect(scope.order_reverse).toBeTruthy();         });
-    it("should set 'order_col' to 'bounty_total'",  function() {expect(scope.order_col).toEqual("bounty_total");  });
-
-    xdescribe("Ctrl Locals::", function() {
-      it("should set 'new_tag'", function() {
-        expect(tracker_ctrl.tracker).toBeDefined();
-      });
-    });
-
-  });
-  xdescribe("AFTER tracker recieved", function() {
-    it("should respond when NOT Following", function() {
-      var unfollowed_tracker = get_new_object(mock_tracker);
-      expect(unfollowed_tracker).not.toBeNull();
+    // cant mock out too much interdependency can;t access inner function
+    xit("should have a create_payment function", function() {
+      var issue_id = jasmine.any(Number);
+      scope.issue(issue_id, jasmine.any(Function));
+      expect(scope.create_payment).toBeDefined();
     });
   });
 });
