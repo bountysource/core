fd8db53ecb4d406d9d57c3ce9a5d55b8db9d377b
Cory Boyd <corytheboyd@gmail.com>
2013-07-14 16:07:16 -0700

    (admin) access token override

--- app/pages/layout/navbar.js
+++ app/pages/layout/navbar.js
@@ -1,10 +1,29 @@
 'use strict';
 
 angular.module('app')
-  .controller('Navbar', function ($scope, $api) {
+  .controller('Navbar', function ($scope, $cookieStore, $window, $api) {
     $scope.setEnv = $api.setEnvironment;
+
+    $scope.set_access_token = {
+      show_modal: false,
+      new_token: null,
+      open: function() {
+        this.show_modal = true;
+      },
+      close: function() {
+        this.show_modal = false;
+      },
+      save: function() {
+        $cookieStore.put($api.access_token_cookie_name, this.new_token);
+        $api.load_current_person_from_cookies();
+        this.close();
+      }
+    };
   })
+
   .controller('NavbarLinkedAccountSignin', function($scope, $location, $api) {
+    window.scope = $scope;
+
     $scope.save_route = function() {
       $api.set_post_auth_url($location.url());
     };
