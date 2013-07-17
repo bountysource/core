6a41b0338e5aadd25b2d90714e126519a01beafe
Cory Boyd <corytheboyd@gmail.com>
2013-07-17 12:12:31 -0700

    testing gittip

--- app/pages/auth/auth.js
+++ app/pages/auth/auth.js
@@ -11,33 +11,33 @@ angular.module('app')
   })
   .controller("AuthConfirmController", function($scope, $routeParams, $window, $location, $api) {
     $scope.provider = $routeParams.provider;
-    console.log("auth confirm", $routeParams.provider);
+    console.log("auth confirm", $routeParams.provider, $scope.current_person);
 
-    $scope.accept = function() {
-      $scope.pending_connect = true;
-
-      var connect_params = angular.copy($routeParams);
-      delete connect_params.provider;
-
-      $api.approve_connect($scope.provider, connect_params).then(function(response) {
-        if (response.error) {
-          $scope.error = response.error;
-        } else {
-          $scope.pending_connect = false;
-          $scope.redirecting = true;
-
-          $window.setTimeout(function() {
-            $window.location = response.redirect_url;
-          }, 1500);
-        }
-      });
-    };
-
-    $scope.reject = function() {
-      if ($routeParams.redirect_url) {
-        $window.location = $routeParams.redirect_url;
-      } else {
-        $location.url("/");
-      }
-    };
+//    $scope.accept = function() {
+//      $scope.pending_connect = true;
+//
+//      var connect_params = angular.copy($routeParams);
+//      delete connect_params.provider;
+//
+//      $api.approve_connect($scope.provider, connect_params).then(function(response) {
+//        if (response.error) {
+//          $scope.error = response.error;
+//        } else {
+//          $scope.pending_connect = false;
+//          $scope.redirecting = true;
+//
+//          $window.setTimeout(function() {
+//            $window.location = response.redirect_url;
+//          }, 1500);
+//        }
+//      });
+//    };
+//
+//    $scope.reject = function() {
+//      if ($routeParams.redirect_url) {
+//        $window.location = $routeParams.redirect_url;
+//      } else {
+//        $location.url("/");
+//      }
+//    };
   });
