bf3a7370cc8b8852e8be180205cb645b6ab258e7
Cory Boyd <corytheboyd@gmail.com>
2013-07-17 12:14:57 -0700

    testing gittip AGAIN

--- app/pages/auth/auth.js
+++ app/pages/auth/auth.js
@@ -4,7 +4,7 @@ angular.module('app')
   .config(function($routeProvider, $person) {
     $routeProvider
       .when("/auth/:provider/confirm", {
-        templateUrl: "/pages/auth/auth.html",
+        templateUrl: "pages/auth/auth.html",
         controller: "AuthConfirmController",
         resolve: $person
       });
@@ -13,31 +13,31 @@ angular.module('app')
     $scope.provider = $routeParams.provider;
     console.log("auth confirm", $routeParams.provider, $scope.current_person);
 
-//    $scope.accept = function() {
-//      $scope.pending_connect = true;
-//
-//      var connect_params = angular.copy($routeParams);
-//      delete connect_params.provider;
-//
-//      $api.approve_connect($scope.provider, connect_params).then(function(response) {
-//        if (response.error) {
-//          $scope.error = response.error;
-//        } else {
-//          $scope.pending_connect = false;
-//          $scope.redirecting = true;
-//
-//          $window.setTimeout(function() {
-//            $window.location = response.redirect_url;
-//          }, 1500);
-//        }
-//      });
-//    };
-//
-//    $scope.reject = function() {
-//      if ($routeParams.redirect_url) {
-//        $window.location = $routeParams.redirect_url;
-//      } else {
-//        $location.url("/");
-//      }
-//    };
+    $scope.accept = function() {
+      $scope.pending_connect = true;
+
+      var connect_params = angular.copy($routeParams);
+      delete connect_params.provider;
+
+      $api.approve_connect($scope.provider, connect_params).then(function(response) {
+        if (response.error) {
+          $scope.error = response.error;
+        } else {
+          $scope.pending_connect = false;
+          $scope.redirecting = true;
+
+          $window.setTimeout(function() {
+            $window.location = response.redirect_url;
+          }, 1500);
+        }
+      });
+    };
+
+    $scope.reject = function() {
+      if ($routeParams.redirect_url) {
+        $window.location = $routeParams.redirect_url;
+      } else {
+        $location.url("/");
+      }
+    };
   });
