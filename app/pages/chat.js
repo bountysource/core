6810d723564bd075f9a01260578d7ed5b74c064c
Cory Boyd <corytheboyd@gmail.com>
2013-08-05 12:43:59 -0700

    sanitize nickname for chatroom

--- app/pages/chat.js
+++ app/pages/chat.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('app')
-  .controller('ChatController', function ($rootScope) {
+  .controller('ChatController', function ($rootScope, $scope) {
     // this really doesn't belong here
     $rootScope.chatroom = {
       show: false,
@@ -13,7 +13,11 @@ angular.module('app')
 
     $rootScope.$watch("current_person", function(current_person) {
       if (current_person) {
-        $rootScope.chatroom.nick = current_person.display_name;
+        $rootScope.chatroom.nick = $scope.sanitize_nick(current_person.display_name);
       }
     });
+
+    $scope.sanitize_nick = function(nick) {
+      return nick.replace(/\s+/g,'_').replace(/[^a-z0-9-_]/ig,'');
+    };
   });
