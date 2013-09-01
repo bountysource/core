8f3810857e128c67a1c0057c334aea3cdeef3376
MaksJS <maxigamer59@hotmail.com>
2013-09-01 17:48:11 +0200

    fix #42 - twitter cards
    Added a angular service called $metaTags to dynamically append twitter
    meta tags
    Twitter card need to be validated on
    https://dev.twitter.com/docs/cards/validation/validator with
    @bountysource twitter account

--- app/pages/people/activity.js
+++ app/pages/people/activity.js
@@ -8,12 +8,29 @@ angular.module('app')
         controller: 'PeopleShow'
       });
   })
-  .controller('PeopleShow', function ($scope, $routeParams, $api, $pageTitle) {
+  .controller('PeopleShow', function ($scope, $routeParams, $api, $pageTitle, $metaTags) {
 
     $scope.person = $api.person_get($routeParams.id);
 
     $scope.person.then(function(person){
       $pageTitle.set(person.display_name, 'Profile');
+      $metaTags.add({
+        'twitter:card':                'photo',
+        'twitter:site':                '@bountysource',
+        'twitter:creator':             '',
+        'twitter:title':               person.display_name,
+        'twitter:image:src':           person.large_image_url,
+        'twitter:domain':              'bountysource.com',
+        'twitter:app:name:iphone':     '',
+        'twitter:app:name:ipad':       '',
+        'twitter:app:name:googleplay': '',
+        'twitter:app:url:iphone':      '',
+        'twitter:app:url:ipad':        '',
+        'twitter:app:url:googleplay':  '',
+        'twitter:app:id:iphone':       '',
+        'twitter:app:id:ipad':         '',
+        'twitter:app:id:googleplay':   ''
+      });
     });
 
     $scope.timeline = $api.person_timeline_get($routeParams.id).then(function(response) {
