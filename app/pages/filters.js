eaf0f30436a65c8d188ba6a3e32ff1754b31301c
Warren Konkel <wkonkel@gmail.com>
2013-09-11 13:34:57 -0700

    add created_at as a sortable column

--- app/pages/filters.js
+++ app/pages/filters.js
@@ -7,8 +7,9 @@ angular.module('app').
     };
   }).filter('dollars', function ($filter) {
     var currency = $filter('currency');
-    return function(input) {
-      return currency(input, '$').replace(/\.\d\d$/,'');
+    return function(input, options) {
+      options = options || {};
+      return currency(input, (options.space ? '$ ' : '$')).replace(/\.\d\d$/,'');
     };
   }).filter('round', function () {
     return function (input) {
@@ -96,6 +97,32 @@ angular.module('app').
       // pluralize the unit if necessary
       if (time_left !== 1) { time_left_unit += "s"; }
       return time_left + " " + time_left_unit;
+    };
+  }).filter('time_ago_short', function() {
+    return function(date, options) {
+      options = options || {};
+      var now = new Moment();
+      var ends = new Moment(date);
+      if (ends < now) {
+        var tmp = now;
+        now = ends;
+        ends = tmp;
+      }
+      var years_left = ends.diff(now, "years");
+      var months_left = ends.diff(now, "months");
+      var days_left = ends.diff(now, "days");
+      var time_left, time_left_unit;
+      if (years_left > 0) {
+        time_left = years_left;
+        time_left_unit = "yr";
+      } else if (months_left > 0) {
+        time_left = months_left;
+        time_left_unit = "mo";
+      } else {
+        time_left = days_left;
+        time_left_unit = "d";
+      }
+      return time_left + ' ' + time_left_unit;
     };
   });
 
