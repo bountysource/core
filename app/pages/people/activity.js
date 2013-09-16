641811aba0dfedd1a811a6e3d787a4ad62d4282e
Cory Boyd <corytheboyd@gmail.com>
2013-09-15 17:21:08 -0700

    filthy hack to redirect to teams page for Mozilla, Uber, Adobe, and Gust

--- app/pages/people/activity.js
+++ app/pages/people/activity.js
@@ -8,11 +8,22 @@ angular.module('app')
         controller: 'PeopleShow'
       });
   })
-  .controller('PeopleShow', function ($scope, $routeParams, $api, $pageTitle) {
+  .controller('PeopleShow', function ($scope, $routeParams, $api, $pageTitle, $location) {
     $scope.person = $api.person_get($routeParams.id);
 
     $scope.person.then(function(person){
       $pageTitle.set(person.display_name, 'Profile');
+
+      // revolting hack to redirect to Team page instead of Person page.
+      if (person.id === 19571) {
+        $location.path("/teams/mozilla/activity").replace();
+      } else if (person.id === 18619) {
+        $location.path("/teams/adobe/activity").replace();
+      } else if (person.id === 19091) {
+        $location.path("/teams/gust/activity").replace();
+      } else if (person.id === 17691) {
+        $location.path("/teams/uber/activity").replace();
+      }
     });
 
     $scope.timeline = $api.person_activity($routeParams.id);
