865b3ad29dae34336ba778a0ab8024dbe501bb82
Cory Boyd <corytheboyd@gmail.com>
2013-09-18 16:49:30 -0700

    add new members to make 4 slots. mobile friendly. add directive for owner href based on type

--- app/pages/directives.js
+++ app/pages/directives.js
@@ -218,4 +218,20 @@ angular.module('app').
         });
       }
     };
-  }]);
+  }])
+  .directive('ownerHref', function() {
+    return {
+      restrict: "AC",
+      link: function(scope, element, attr) {
+        scope.$watch(attr.ownerHref, function(owner) {
+          if (owner) {
+            if ((/^person$/i).test(owner.type)) {
+              element.attr("href", "/people/"+owner.slug);
+            } else if ((/^team$/i).test(owner.type)) {
+              element.attr("href", "/teams/"+owner.slug);
+            }
+          }
+        });
+      }
+    };
+  });
