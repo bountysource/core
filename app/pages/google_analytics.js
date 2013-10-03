9b63d8e5f48ace89e46a2c7430efbf38d59b9020
Warren Konkel <wkonkel@gmail.com>
2013-10-03 11:12:19 -0700

    plugin url changed

--- app/pages/google_analytics.js
+++ app/pages/google_analytics.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window._gaq = window._gaq || [];
-window._gaq.push(['_require', 'inpage_linkid', '//www.google-analytics.com/tools/ga/inpage_linkid.js']);
+window._gaq.push(['_require', 'inpage_linkid', '//www.google-analytics.com/plugins/ga/inpage_linkid.js']);
 window._gaq.push(['_setAccount', document.location.host === 'www.bountysource.com' ? 'UA-36879724-1' : 'UA-36879724-2']);
 (function(d,t){
   var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
@@ -10,7 +10,9 @@ window._gaq.push(['_setAccount', document.location.host === 'www.bountysource.co
 }(document,'script'));
 
 angular.module('app').run(function($rootScope, $window, $location) {
+  console.log("firing google analytics pixel");
   $rootScope.$on('$viewContentLoaded', function() {
+    console.log("really firing google analytics pixel");
     $window._gaq.push(['_trackPageview', $location.url()]);
   });
 });
