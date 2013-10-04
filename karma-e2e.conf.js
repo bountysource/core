6c4fb7c24e772830bd67226d9b2e24358eaf1af2
zachgersh <zdgman@gmail.com>
2013-10-03 17:03:35 -0700

    reconfigures karma a tiny bit'

--- karma-e2e.conf.js
+++ karma-e2e.conf.js
@@ -1,66 +1,73 @@
 // Karma E2E configuration
+module.exports = function(config) {
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+    // list of files / patterns to load in the browser
+    files: [
+      'test/e2e/mock.js',
+      'test/e2e/helper.js',
+      'test/e2e/signin_e2e.js',
+      'test/e2e/fundraiser_create_e2e.js',
+      'test/e2e/team_e2e.js',
+      'test/e2e/bounty_create_e2e.js',
+      'test/e2e/profile_edit_e2e.js'
+    ],
 
-// list of files / patterns to load in the browser
-files = [
-  ANGULAR_SCENARIO,
-  ANGULAR_SCENARIO_ADAPTER,
-  'test/e2e/mock.js',
-  'test/e2e/helper.js',
-  'test/e2e/signin_e2e.js',
-  'test/e2e/fundraiser_create_e2e.js',
-  'test/e2e/team_e2e.js',
-  'test/e2e/bounty_create_e2e.js',
-  'test/e2e/profile_edit_e2e.js'
-];
-frameworks = ["ng-scenario"];
+    frameworks: ["ng-scenario"],
 
-// list of files to exclude
-exclude = [];
+    // list of files to exclude
+    exclude: [],
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['dots'];
+    // test results reporter to use
+    // possible values: dots || progress || growl
+    reporters: ['dots', 'progress'],
 
-// web server port
-port = 8081;
+    // web server port
+    port: 8081,
 
-// cli runner port
-runnerPort = 9101;
+    // cli runner port
+    runnerPort: 9101,
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
 
-proxies = {
-  '/': 'http://localhost:9001/'
+    proxies: {
+      '/': 'http://localhost:9001/'
+    },
+
+    urlRoot:  "/_karma_/",
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+
+    //browsers = ['Firefox', 'Safari', 'Chrome'];
+    browsers: ['PhantomJS'],
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 5000,
+
+    // Continuous Integration mode
+    singleRun: true,
+    // if true, it capture browsers, run tests and exit
+
+    plugins: [
+      'karma-phantomjs-launcher',
+      'karma-ng-scenario'
+    ]
+  });
 };
-
-urlRoot = "/_karma_/";
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-
-//browsers = ['Firefox', 'Safari', 'Chrome'];
-browsers = ['PhantomJS'];
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
-
-// Continuous Integration mode
-singleRun = true;
-// if true, it capture browsers, run tests and exit
---
+++ test/unit/controller/fundraiser/indexSpec.js
@@ -0,0 +1,5 @@
+'use strict';
+
+describe('FundraiserIndexController', function() {
+
+});
