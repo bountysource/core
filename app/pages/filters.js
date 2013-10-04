2de75f1cd1fb076d5c7a84d1ef3e575856c2e167
Drew Kitch <drew.kitch@gmail.com>
2013-10-03 17:11:00 -0700

    github plugin frontend

--- app/pages/filters.js
+++ app/pages/filters.js
@@ -147,6 +147,26 @@ angular.module('app').
       }
       return retval;
     };
+  }).filter('orderObjectBy', function() {
+    return function(items, field, reverse) {
+      var filtered = [];
+      angular.forEach(items, function(item) {
+        filtered.push(item);
+      });
+      filtered.sort(function (a, b) {
+        if (a[field] > b[field]) {
+          return 1;
+        }
+        if (a[field] < b[field]) {
+          return -1;
+        }
+        return 0;
+      });
+      if (reverse) {
+        filtered.reverse();
+      }
+      return filtered;
+    };
   }).filter('hex', function() {
     return function(input) {
       if (input) {
