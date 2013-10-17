1f75667e873896719be914795e927b9834775693
zachgersh <zdgman@gmail.com>
2013-10-17 15:49:57 -0700

    removes some temp code, steals partial from people

--- app/pages/dash/dash.js
+++ app/pages/dash/dash.js
@@ -4,12 +4,15 @@ angular.module('app')
   .config(function($routeProvider, $person) {
     $routeProvider
     .when('/suchwowdashboard', {
-      template_url: 'pages/dash/dash.html',
+      templateUrl: 'pages/dash/dash.html',
       controller: 'Dash',
       resolve: $person
   });
 })
 
 .controller('Dash', function($scope, $location, $api) {
+  $scope.person = $api.person_get($scope.current_person.id);
+  $scope.teams = $api.person_teams_get($scope.current_person.id);
   console.log('wow');
+  console.log($scope.current_person);
 });
