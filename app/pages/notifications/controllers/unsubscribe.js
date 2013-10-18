2bb391074ad0154a9ac0f06f96fb6e014a98ec28
Drew Kitch <drew.kitch@gmail.com>
2013-10-18 13:43:20 -0700

    in progress

---
+++ app/pages/notifications/controllers/unsubscribe.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('app')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/notifications/unsubscribe', {
+        templateUrl: 'pages/notifications/unsubscribe.html',
+        controller: 'NotificationsController',
+        title: 'Unsubscribe from Bountysource Emails'
+      });
+  }).controller('NotificationsController', function($routeParams, $scope, $api) {
+    var email_address = $routeParams.email;
+    var email_list = $routeParams.type;
+    var data = {
+      email: email_address,
+      type: email_list
+    };
+    $scope.response = $api.call("/notifications/unsubscribe", 'POST', data);
+  });
