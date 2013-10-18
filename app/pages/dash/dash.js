1fea4bece34a43edbc13aa624ef1444df5a9e785
zachgersh <zdgman@gmail.com>
2013-10-18 15:02:54 -0700

    adds styling and some fake objects to this dash

--- app/pages/dash/dash.js
+++ app/pages/dash/dash.js
@@ -7,17 +7,35 @@ angular.module('app')
       templateUrl: 'pages/dash/dash.html',
       controller: 'Dash',
       resolve: $person
-  });
-})
+    });
+  })
 
 .controller('Dash', function($scope, $location, $api) {
   //supposed to be some sort of api response
-  var favLang = ['ruby', 'java', 'clojure']
+  var favLang = ['ruby', 'java', 'clojure'];
 
   $scope.person = $api.person_get($scope.current_person.id);
   $scope.teams = $api.person_teams_get($scope.current_person.id);
 
   $scope.issues = $api.search(favLang[0]).then(function(data) {
-    return data.issues.slice(0,3)
+    return data.issues.slice(0,3);
   });
+
+  $scope.myBugs = {
+    issue1: {
+      project: "Ruby on Rails",
+      title: "Including ActionController::Live causes a controller to swallow all errors and return 200 always",
+      url: "/issues/866811-including-actioncontroller-live-causes-a-controller-to-swallow-all-errors-and-return-200-always"
+    },
+    issue2: {
+      project: "Ruby on Rails",
+      title: "Major performance regression when preloading has_many_through association",
+      url: "/issues/866819-major-performance-regression-when-preloading-has_many_through-association"
+    },
+    issue3: {
+      project: "Ruby on Rails",
+      title: "Serialized attributes always get marked as changed by ActiveModel::Dirty in new record",
+      url: "/issues/866827-serialized-attributes-always-get-marked-as-changed-by-activemodel-dirty-in-new-record"
+    }
+  };
 });
