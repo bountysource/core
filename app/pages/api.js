d6ede242997048a45ef5976374c2ddc6fd04e9e9
Drew Kitch <drew.kitch@gmail.com>
2013-10-31 13:32:09 -0700

    adds bounty claims to people/:id/activity timeline

--- app/pages/api.js
+++ app/pages/api.js
@@ -257,12 +257,12 @@ angular.module('api.bountysource',[]).
         for (i in activity.pledges) { timeline.push(activity.pledges[i]); }
         for (i in activity.fundraisers) { timeline.push(activity.fundraisers[i]); }
         for (i in activity.teams) { timeline.push(activity.teams[i]); }
+        for (i in activity.bounty_claims) {timeline.push(activity.bounty_claims[i]); }
 
         // add sort date since the col is either added_at (teams) or created_at (everything else)
         for (i in timeline) {
           timeline[i].sort_date = timeline[i].added_at || timeline[i].created_at;
         }
-
         return timeline;
       });
     };
