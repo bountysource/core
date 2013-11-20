17149abefc2b7aabdd4976c61d13cc3906e7cdb2
Sean Miller <smiller@bountysource.com>
2013-11-20 15:41:30 -0800

    Add bounties tab to teams view

---
+++ app/pages/teams/bounties.js
@@ -0,0 +1,51 @@
+'use strict';
+
+angular.module('app')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/teams/:id/bounties', {
+        templateUrl: 'pages/teams/bounties.html',
+        controller: 'BaseTeamController'
+      });
+  })
+  .controller('TeamBountiesController', function ($scope, $routeParams, $api) {
+    $scope.bounties_resolved = false;
+
+    $scope.open_sort = {
+      column: "created_at",
+      desc: true
+    };
+    $scope.claimed_sort = {
+      column: "issue.paid_at",
+      desc: true
+    };
+    $scope.update_sort = function(obj, column) {
+      if (obj.column === column) {
+        obj.desc = !obj.desc;
+      } else {
+        obj.column = column;
+        obj.desc = true;
+      }
+    };
+
+    $api.team_bounties($routeParams.id).then(function(bounties) {
+      var claimed_bounties = [];
+      var open_bounties = [];
+      var total_paid_out = 0;
+      for (var i=0; i<bounties.length; i++) {
+        var bounty = bounties[i];
+        bounty.amount = parseInt(bounty.amount, 10);
+        if (bounty.issue.paid_out) {
+          claimed_bounties.push(bounty);
+          total_paid_out += bounty.amount;
+        } else {
+          open_bounties.push(bounty);
+        }
+      }
+      $scope.claimed_bounties = claimed_bounties;
+      $scope.open_bounties = open_bounties;
+      $scope.total_paid_out = total_paid_out;
+
+      $scope.bounties_resolved = true;
+    });
+  });
