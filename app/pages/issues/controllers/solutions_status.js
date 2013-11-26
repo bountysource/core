29ce21fca2745f21e128729b33b7c7ac177dca4d
Cory Boyd <corytheboyd@gmail.com>
2013-11-26 14:03:19 -0800

    use base controller. add your solution to devs working array on create

--- app/pages/issues/controllers/solutions_status.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('app')
-  .controller('SolutionsStatusController', function ($scope, $routeParams, $api) {
-    $api.solutions_get($routeParams.id).then(function (response) {
-      $scope.solutions = response;
-      console.log("solutions ", $scope.solutions);
-    });
-  });
