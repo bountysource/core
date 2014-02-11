0bd73586896b04f8441082d42538ff666bd2bac7
Sean Miller <smiller@bountysource.com>
2014-02-10 16:43:29 -0800

    Adds progress bars to team/:id/bounties page

--- app/pages/teams/bounties.js
+++ app/pages/teams/bounties.js
@@ -17,7 +17,7 @@ angular.module('app')
     };
 
     $scope.open_sort = {
-      column: "created_at",
+      column: "progress_sum",
       desc: true
     };
 
@@ -77,6 +77,7 @@ angular.module('app')
           total_paid_out += bounty.amount;
           incrementStats(bounty, bounty.issue.paid_out);
         } else {
+          bounty = setProgressAttributes(bounty);
           open_bounties.push(bounty);
           total_open += bounty.amount;
           incrementStats(bounty, false);
@@ -97,5 +98,70 @@ angular.module('app')
       $scope.total_open = total_open;
       $scope.bounties_resolved = true;
     });
+
+    function setProgressAttributes (bounty) {
+      var value_array = [],
+          key,
+          issue = bounty.issue;
+
+      if(bounty.amount > 0) {
+        bounty.status = "You've placed a bounty on this issue. ";
+      }
+
+      for(key in issue) {
+        //loop through elements in issue
+        switch(key) {
+        case "developer_goals":
+          if(issue.developer_goals.length > 0) {
+            bounty.status += "A developer goal has been set.";
+          }
+          break;
+        case "solutions":
+          if(issue.solutions.length > 0) {
+            bounty.status += "A solution is in progress. ";
+          }
+          break;
+        case "can_add_bounty":
+          if(!issue.can_add_bounty) {
+            bounty.status += "The issue has been closed. ";
+          }
+          break;
+        case "bounty_claims":
+          if(issue.bounty_claims.length > 0) {
+            bounty.status += "A bounty claim has been submitted. ";
+          }
+          break;
+        }
+
+      }
+      // set attributes on the bounty object
+      bounty = setBountyStatus(bounty);
+      return bounty;
+    }
+
+    function setBountyStatus (bounty) {
+      var final_status, final_progress, title;
+      if(/claim/.test(bounty.status)) {
+        title = "A developer has claimed the bounty for this issue.";
+        final_status = "If you haven’t already, please vote on the claim on the Issue page.";
+        final_progress = 80;
+      } else if (/closed/.test(bounty.status)) {
+        title = "This issue has been closed.";
+        final_status = "The developer who solved this issue needs to claim the bounty.";
+        final_progress = 60;
+      } else if (/solution/.test(bounty.status)) {
+        title = "A developer is working on a solution for this issue.";
+        final_status = "You can check on the status of the solution on the Issue page.";
+        final_progress = 40;
+      } else if (/goal/.test(bounty.status)) {
+        title = "A developer has set a bounty goal for this issue.";
+        final_status = "Increase the bounty to match the developer’s bounty goal!";
+        final_progress = 30;
+      }
+      bounty.status = final_status || "Go spread the word on Twitter, Facebook, or Google+!";
+      bounty.title = title || "You have posted a bounty on this issue.";
+      bounty.progress_sum = final_progress || 20;
+      return bounty;
+    }
 
   });
