meese/frontend

no more assigning scope vars to promise objects

app/pages/teams/index.js
@@ -12,5 +12,8 @@ angular.module('app')
   .controller('TeamsIndexController', function ($scope, $location, $api, $pageTitle) {
     $pageTitle.set("Teams");
 
-    $scope.teams = $api.list_teams();
+    $api.list_teams().then(function(teams) {
+      $scope.teams = teams;
+      return teams;
+    });
   });

bountysource/core

app/pages/teams/index.js
@@ -12,5 +12,8 @@ angular.module('app')
   .controller('TeamsIndexController', function ($scope, $location, $api, $pageTitle) {
     $pageTitle.set("Teams");
 
-    $scope.teams = $api.featured_teams();
+    $api.featured_teams().then(function(teams) {
+      $scope.teams = teams;
+      return teams;
+    });
   });