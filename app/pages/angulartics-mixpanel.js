3b78cad4894c07784b3c469396866a8f1774c037
Cory Boyd <corytheboyd@gmail.com>
2014-03-06 16:09:54 -0800

    disable global page tracking. extract common events into service

--- app/pages/angulartics-mixpanel.js
+++ app/pages/angulartics-mixpanel.js
@@ -2,11 +2,13 @@
 
 angular.module('angulartics.mixpanel', ['angulartics'])
   .config(['$analyticsProvider', function ($analyticsProvider) {
-    angulartics.waitForVendorApi('mixpanel', 500, function (mixpanel) {
-      $analyticsProvider.registerPageTrack(function (path) {
-        mixpanel.track( "Page Viewed", { "page": path } );
-      });
-    });
+
+//    Disabling global page tracking for now
+//    angulartics.waitForVendorApi('mixpanel', 500, function (mixpanel) {
+//      $analyticsProvider.registerPageTrack(function (path) {
+//        mixpanel.track( "Page Viewed", { "page": path } );
+//      });
+//    });
 
     angulartics.waitForVendorApi('mixpanel', 500, function (mixpanel) {
       $analyticsProvider.registerEventTrack(function (action, properties) {
@@ -15,11 +17,61 @@ angular.module('angulartics.mixpanel', ['angulartics'])
     });
   }]);
 
+angular.module('app').service('mixpanelEvent', function($analytics) {
+
+  /*
+   * Track Issue view
+   * */
+  this.issueView = function(id, options) {
+    options = options || {};
+    var payload = angular.extend(options, { id: id });
+    $analytics.eventTrack('Issue View', payload);
+  };
+
+  /*
+   * Track Fundraiser view
+   * */
+  this.fundraiserView = function(id, options) {
+    options = options || {};
+    var payload = angular.extend(options, { id: id });
+    $analytics.eventTrack('Fundraiser View', payload);
+  };
+
+  /*
+  * Track Tracker view
+  * */
+  this.trackerView = function(id, options) {
+    options = options || {};
+    var payload = angular.extend(options, { id: id });
+    $analytics.eventTrack('Tracker View', payload);
+  }
+
+  /*
+   * Start the Bounty placement process.
+   * */
+  this.bountyStart = function(options) {
+    options = options || {};
+    var payload = angular.extend(options, { type: '$direct' });
+    $analytics.eventTrack('Bounty Start', payload);
+  };
+
+  /*
+   * Start the Pledge placement process.
+   * */
+  this.pledgeStart = function(options) {
+    options = options || {};
+    var payload = angular.extend(options, { type: '$direct' });
+    $analytics.eventTrack('Pledge Start', payload);
+  };
+
+});
+
+
 /*
 * Fire Mixpanel events based on route
 * */
 angular.module('app')
-  .run(function($rootScope, $location, $analytics) {
+  .run(function($rootScope, $location, mixpanelEvent) {
 
     $rootScope.$on('$routeChangeSuccess', function(routeChangeEvent, currentRoute) {
       var path = $location.path();
@@ -28,27 +80,21 @@ angular.module('app')
        * Issue show page views
        * */
       if (/^\/issues.+/.test(path)) {
-        $analytics.eventTrack('Issue View', {
-          id: currentRoute.params.id
-        });
+        mixpanelEvent.issueView(currentRoute.params.id);
       }
 
       /*
       * Fundraiser show page views
       * */
       if (/^\/fundraisers.+/.test(path)) {
-        $analytics.eventTrack('Fundraiser View', {
-          id: currentRoute.params.id
-        });
+        mixpanelEvent.fundraiserView(currentRoute.params.id);
       }
 
       /*
       * Tracker show page view
       * */
       if (/^\/trackers.+/.test(path)) {
-        $analytics.eventTrack('Tracker View', {
-          id: currentRoute.params.id
-        });
+        mixpanelEvent.trackerView(currentRoute.params.id);
       }
 
     });
