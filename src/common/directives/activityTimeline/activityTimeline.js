e0c5c9348607d7cf552a56b8ee13dfbfc8880428
Sean Miller <smiller@bountysource.com>
2014-03-13 13:01:19 -0700

    Creates activityTimeline directive.

---
+++ src/common/directives/activityTimeline/activityTimeline.js
@@ -0,0 +1,9 @@
+angular.module('directives').directive('activityTimeline', function () {
+  return {
+    restrict: 'EA',
+    templateUrl: 'common/directives/activityTimeline/templates/activityTimeline.html',
+    scope: {
+      timeline: "="
+    }
+  }
+});
