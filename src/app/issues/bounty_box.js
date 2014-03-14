ff33194c6bf0a1d49c4e7d3050277bdcbb08cd8e
Sean Miller <smiller@bountysource.com>
2014-03-14 11:47:09 -0700

    Re-adds mixpanel events for bounty-start: on tab, custom input, and obama buttons

--- src/app/issues/bounty_box.js
+++ src/app/issues/bounty_box.js
@@ -1,14 +1,24 @@
 'use strict';
 
-angular.module('app').controller('BountyBoxController', function ($scope, $routeParams, $window, $location) {
+angular.module('app').controller('BountyBoxController', function ($scope, $routeParams, $window, $location, mixpanelEvent) {
   $scope.bounty = {
     amount: parseInt($routeParams.amount, 10)
   };
 
   $scope.place_bounty_redirect = function(amount) {
-    amount = amount || $scope.bounty.amount;
-    if (angular.isNumber(amount)) {
-      $location.path("/issues/"+$routeParams.id+"/bounty").search({ amount: amount });
+     amount = amount || $scope.bounty.amount;
+     // Track bounty start event in Mixpanel
+     mixpanelEvent.bountyStart({ type: 'buttons', amount: amount });
+     if (angular.isNumber(amount)) {
+       $location.path("/issues/"+$routeParams.id+"/bounty").search({ amount: amount });
+     }
+   };
+
+  $scope.custom_bounty_redirect = function() {
+    mixpanelEvent.bountyStart({ type: 'custom', amount: $scope.bounty.amount });
+    if (angular.isNumber($scope.bounty.amount)) {
+      $location.path("/issues/"+$routeParams.id+"/bounty").search({ amount: $scope.bounty.amount });
     }
   };
+
 });
