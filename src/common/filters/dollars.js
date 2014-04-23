b18a89d365829c1a676b0c9b47880f5bfb3a9738
Cory Boyd <corytheboyd@gmail.com>
2014-04-23 11:39:38 -0700

    dollars filter more angular-y. amount in bounty create button. move currency switcher, remove tooltip from navbar

--- src/common/filters/dollars.js
+++ src/common/filters/dollars.js
@@ -1,24 +1,35 @@
 'use strict';
 
+/*
+* Show currency value with correct unit.
+* @param displayCurrency the currency to show
+* @param actualCurrency the currency of value. if not the same as displayCurrency, then convert amount.
+* */
 angular.module('filters').filter('dollars', function($filter, $api, $currency) {
-  return function(value, options) {
-    options = options || {};
+  return function(value, actualCurrency, displayCurrency) {
+    displayCurrency = displayCurrency || $currency.value;
+    actualCurrency = actualCurrency || 'USD';
 
-    if ($currency.value === 'BTC') {
-      value = $currency.usdToBtc(value);
-      if (value <= 0 || value > 0.001) {
-        return (options.space ? '฿ ' : '฿') + $filter('number')(value, 3);
+    var unit = $filter('currencyUnit')(value, displayCurrency);
 
-      // 1 mBTC = 0.001 BTC
-      } else if (value <= 0.001 && value > 0.000001) {
-        return 'mBTC ' + $filter('number')(value * 1000, 3);
-
-      // 1 uBTC = 0.000001 BTC
-      } else {
-        return 'μBTC ' + $filter('number')(value * 1000000, 3);
+    // Convert amount if display and actual currencies do not match
+    if (displayCurrency !== actualCurrency) {
+      if (actualCurrency === 'USD' && displayCurrency === 'BTC') {
+        value = $currency.usdToBtc(value);
       }
-    } else {
-      return (options.space ? '$ ' : '$') + $filter('number')(value, 0);
+    }
+
+    if (displayCurrency === 'USD') {
+      return unit + $filter('number')(value, 0);
+
+    } else if (displayCurrency === 'BTC') {
+      if (value <= 0.001 && value > 0.000001) {
+        value *= 1000; // mBTC
+      } else if (value <= 0.000001) {
+        value *= 1000000; // uBTC
+      }
+
+      return unit + $filter('number')(value, 3);
     }
   };
 });
