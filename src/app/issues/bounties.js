778025d6e51b90acc76dd63c2b8536d15f775da4
Sean Miller <smiller@bountysource.com>
2014-04-28 12:56:17 -0700

    Use v2 for issue/bounties page. Adds inteceptor to Bounties resource

--- src/app/issues/bounties.js
+++ src/app/issues/bounties.js
@@ -1,9 +1,25 @@
 'use strict';
 
-angular.module('app').controller('IssueBountiesController', function ($scope, $routeParams, $api) {
-  $api.issue_get($routeParams.id).then(function(issue) {
-    $scope.issue = issue;
-    return issue;
+angular.module('app').controller('IssueBountiesController', function ($scope, $routeParams, $api, Issue, Bounties, $pageTitle) {
+  // Load issue object
+  $scope.issue = Issue.get({
+    id: $routeParams.id,
+    include_body_html: true,
+    include_author: true,
+    include_tracker: true
+  }, function (issue) {
+    $pageTitle.set(issue.title, issue.tracker.name);
+  });
+
+  // Load issue bounties
+  $scope.bounties = Bounties.get({
+    issue_id: $routeParams.id,
+    include_owner: true,
+    order: '+amount'
+  }, function (bounties) {
+    $scope.issue.$promise.then(function (issue) {
+      issue.bounties = bounties;
+    });
   });
 
   $scope.sort_column = 'amount';
