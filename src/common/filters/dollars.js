217c2bcfe00b481b62cac32012b487697b04f467
Sean Miller <smiller@bountysource.com>
2014-05-08 10:17:55 -0700

    Updated dollar-filter unit option; Said option defaults to true

--- src/common/filters/dollars.js
+++ src/common/filters/dollars.js
@@ -6,7 +6,8 @@
 angular.module('filters').filter('dollars', function($filter, $api, $currency) {
   return function(value, options) {
     options = options || {};
-
+    // if undefined, default rendering currency unit to true
+    if (angular.isUndefined(options.unit)) {options.unit = true;}
     // Force space between unit and value when MSC and XRP
     if ($currency.isMSC() || $currency.isXRP()) {
       options.space = true;
@@ -16,6 +17,6 @@ angular.module('filters').filter('dollars', function($filter, $api, $currency) {
     var unit = $filter('currencyUnit')(value, $currency.value);
     var precision = $currency.precision($currency.value, options.precision || {});
 
-    return (options.hide_unit ? '' : unit) + (options.space ? ' ' : '') + $filter('number')(value, precision);
+    return (options.unit ? unit : '') + (options.space ? ' ' : '') + $filter('number')(value, precision);
   };
 });
