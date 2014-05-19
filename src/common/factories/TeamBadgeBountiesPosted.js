26e502438200377b99b95f7effe359d916857d88
Cory Boyd <corytheboyd@gmail.com>
2014-05-19 11:51:06 -0700

    link to team bounties

--- src/common/factories/TeamBadgeBountiesPosted.js
+++ src/common/factories/TeamBadgeBountiesPosted.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('factories').factory('TeamBadgeBountiesPosted', function (TeamBadge) {
+angular.module('factories').factory('TeamBadgeBountiesPosted', function ($window, TeamBadge) {
 
   var TeamBadgeBountiesPosted = function () {
     this.type = 'bounties_posted';
@@ -9,6 +9,10 @@ angular.module('factories').factory('TeamBadgeBountiesPosted', function (TeamBad
   };
 
   TeamBadgeBountiesPosted.prototype = new TeamBadge();
+
+  TeamBadgeBountiesPosted.prototype.baseFrontendUrl = function () {
+    return $window.location.protocol + '//' + $window.location.host + '/teams/' + this.team.slug + '/bounties';
+  };
 
   TeamBadgeBountiesPosted.prototype.description = function () {
     return 'Bounties posted on behalf of ' + this.team.name;
