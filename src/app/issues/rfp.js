a57b19dffa5257e2f28e79448b0282666ea7ef34
Cory Boyd <corytheboyd@gmail.com>
2014-05-22 16:15:57 -0700

    issues header.html actually holds all of the things in the header

--- src/app/issues/rfp.js
+++ src/app/issues/rfp.js
@@ -3,13 +3,16 @@
 angular.module('app')
   .controller('IssueRequestForProposalsController', function ($scope, $routeParams, $window, $api, $log, Issue, RequestForProposal, Tracker, Team) {
 
+    $scope.resolved = false;
+
     // Find existing RequestForProposal, or build a new
     // instance so that person can submit one (if they are authorized to).
     $scope.issue.$promise.then(function (issue) {
-      RequestForProposal.get({ issue_id: issue.id },
+      RequestForProposal.get({ issue_id: issue.id, include_team: true },
         // Success!
         // The RequestForProposal exists, assign it to $scope.
         function (requestForProposal) {
+          $scope.resolved = true;
           $scope.requestForProposal = requestForProposal;
         },
 
@@ -17,6 +20,7 @@ angular.module('app')
         // Create a new instance of RequestForProposal.
         function (response) {
           if (response.status === 404) {
+            $scope.resolved = true;
             $scope.requestForProposal = new RequestForProposal({ issue_id: $routeParams.id });
           } else {
             $log.error('Unexpected!');
@@ -33,9 +37,8 @@ angular.module('app')
 
     $scope.create = function () {
       if ($window.confirm("Are you sure?")) {
-        $scope.requestForProposal.$save(function (requestForProposal) {
-          // Update the loaded Issue with the RFP
-          $scope.requestForProposal = new RequestForProposal(requestForProposal);
+        $scope.requestForProposal.$save(function () {
+          $scope.requestForProposal.$get({ include_team: true });
         });
       }
     };
