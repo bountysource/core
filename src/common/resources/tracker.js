1452b125dd16dab70d21b8f7a4995a3be47258a3
Cory Boyd <corytheboyd@gmail.com>
2014-05-22 14:20:04 -0700

    touch up tracker factory

--- src/common/resources/tracker.js
+++ src/common/resources/tracker.js
@@ -1,28 +1,29 @@
 "use strict";
 
 angular.module('resources').factory('Tracker', function ($rootScope, $resource) {
-  // define the Tracker resource
-  var tracker = $resource($rootScope.api_host + '/trackers/:id', null, {
-    query: {
-      method: 'GET',
-      isArray: true,
-      headers: { 'Accept': 'application/vnd.bountysource+json; version=2' }
-    }
+
+  var defaultHeaders = { 'Accept': 'application/vnd.bountysource+json; version=2' };
+
+  var Tracker = $resource($rootScope.api_host + '/trackers/:id', { id: '@id' }, {
+    get: { method: 'GET', headers: defaultHeaders },
+    query: { method: 'GET', isArray: true, headers: defaultHeaders }
   });
 
-  // define the tracker module that contains model logic/functions for the Tracker class
-  var trackerModule = {
-    url: function (tracker_ids) {
-      if (this.owner && this.owner.type === "Team") {
-        var tracker_id_params = tracker_ids.join(",");
-        return "/teams/"+this.owner.slug+"/issues?tracker_ids="+tracker_id_params;
-      } else {
-        return "/trackers/"+this.slug;
-      }
+  /*
+  * Get URL for the Tracker.
+  * Cases:
+  * 1. Tracker owned by team. Return URL to the issues tab for the team filtered by this.id
+  * 2. Tracker not owned by team. Return URL to the (legacy?) Tracker page.
+  * */
+  Tracker.prototype.url = function (tracker_ids) {
+    if (this.owner && this.owner.type === "Team") {
+      var tracker_id_params = tracker_ids.join(",");
+      return "/teams/"+this.owner.slug+"/issues?tracker_ids="+tracker_id_params;
+    } else {
+      return "/trackers/"+this.slug;
     }
   };
 
-  // extend the Tracker resource with the tracker module
-  angular.extend(tracker.prototype, trackerModule);
-  return tracker;
+  return Tracker;
+
 });
