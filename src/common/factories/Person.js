cca08a446153092f7550ac44b88106b1b9743a4f
Cory Boyd <corytheboyd@gmail.com>
2014-05-22 14:17:31 -0700

    current_person instance of Person

--- src/common/factories/Person.js
+++ src/common/factories/Person.js
@@ -1,6 +1,6 @@
 'use strcit';
 
-angular.module('factories').factory('Person', function ($rootScope, Team) {
+angular.module('factories').factory('Person', function ($rootScope, $resource) {
 
   var defaultHeaders = { 'Accept': 'application/vnd.bountysource+json; version=2' };
 
@@ -12,12 +12,16 @@ angular.module('factories').factory('Person', function ($rootScope, Team) {
   * Can the Person create an RFP on the issue?
   * */
   Person.prototype.canCreateRfp = function (issue) {
-    // Load Person's teams.
-    Team.query({ person_id: this.id }, function (teams) {
-      debugger;
-    });
+
+    // TODO check permission:
+    // 1. Team has RFP enabled
+    // 2. Person is a member of the team
+    // 3. [Person is an admin of the team ?]
+
+    return true;
+
   };
 
   return Person;
 
-})
+});
