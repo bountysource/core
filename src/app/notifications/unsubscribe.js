2bacd58ab021ddd7b2422da479b3fab990a0e68e
Sean Miller <smiller@bountysource.com>
2014-05-27 17:12:21 -0700

    Allows user to unsubscribe from RFP blast emails either through settings page or unsubscribe page

--- src/app/notifications/unsubscribe.js
+++ src/app/notifications/unsubscribe.js
@@ -10,10 +10,23 @@ angular.module('app').controller('NotificationsController', function($scope, $ro
     $scope.approved = true;
     $scope.processing = true;
 
-    $api.notification_unsubscribe($routeParams.type, { email: $scope.email }).then(function(success) {
-      $scope.processing = false;
-      $scope.success = success;
-      return success;
-    });
+    // hack to use v2 route specifically for proposals
+    if ($routeParams.type === 'proposals') {
+      $api.v2.unsubscribe( { type: $routeParams.type, email: $scope.email , receive_email: false}).then(function (response) {
+        $scope.processing = false;
+        if (response.success) {
+          $scope.success = response;
+        } else {
+          $scope.error = response.data.error;
+        }
+        return response;
+      });
+    } else {
+      $api.notification_unsubscribe($routeParams.type, { email: $scope.email }).then(function(success) {
+        $scope.processing = false;
+        $scope.success = success;
+        return success;
+      });
+    }
   };
 });
