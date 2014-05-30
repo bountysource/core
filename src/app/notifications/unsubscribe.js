8a1eb4df45f48f3735c7d0ffa3be80390b143eac
Sean Miller <smiller@bountysource.com>
2014-05-30 11:35:26 -0700

    Require auth for email unsubscribe

--- src/app/notifications/unsubscribe.js
+++ src/app/notifications/unsubscribe.js
@@ -1,18 +1,22 @@
 'use strict';
 
-angular.module('app').controller('NotificationsController', function($scope, $routeParams, $api, $pageTitle, EmailPreference) {
+angular.module('app').controller('NotificationsController', function($scope, $routeParams, $api, $pageTitle, $location, EmailPreference) {
   $pageTitle.set("Unsubscribe from Bountysource Emails");
 
   $scope.email = $routeParams.email;
   $scope.approved = false;
 
   $scope.unsubscribe = function() {
+    // user must be signed in and have an access token
+
+    $api.require_signin($location.path(), $routeParams);
     $scope.approved = true;
     $scope.processing = true;
 
     // hack to use v2 route specifically for proposals
     if ($routeParams.type === 'proposals') {
-      EmailPreference.update({ type: $routeParams.type, email: $scope.email , receive_email: false}, function (response, headers) {
+      // uses access_token to ID user email
+      EmailPreference.update({ type: $routeParams.type, receive_email: false}, function (response, headers) {
         // success
         $scope.success = true;
         $scope.processing = false;
