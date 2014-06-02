2469072208181c955523b5ebe25ff50e0ef77f76
Sean Miller <smiller@bountysource.com>
2014-06-02 12:22:26 -0700

    Revert "Require auth for email unsubscribe"
    This reverts commit 8a1eb4df45f48f3735c7d0ffa3be80390b143eac.

--- src/app/notifications/unsubscribe.js
+++ src/app/notifications/unsubscribe.js
@@ -1,22 +1,18 @@
 'use strict';
 
-angular.module('app').controller('NotificationsController', function($scope, $routeParams, $api, $pageTitle, $location, EmailPreference) {
+angular.module('app').controller('NotificationsController', function($scope, $routeParams, $api, $pageTitle, EmailPreference) {
   $pageTitle.set("Unsubscribe from Bountysource Emails");
 
   $scope.email = $routeParams.email;
   $scope.approved = false;
 
   $scope.unsubscribe = function() {
-    // user must be signed in and have an access token
-
-    $api.require_signin($location.path(), $routeParams);
     $scope.approved = true;
     $scope.processing = true;
 
     // hack to use v2 route specifically for proposals
     if ($routeParams.type === 'proposals') {
-      // uses access_token to ID user email
-      EmailPreference.update({ type: $routeParams.type, receive_email: false}, function (response, headers) {
+      EmailPreference.update({ type: $routeParams.type, email: $scope.email , receive_email: false}, function (response, headers) {
         // success
         $scope.success = true;
         $scope.processing = false;
