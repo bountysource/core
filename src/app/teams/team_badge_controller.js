c5cd1bf1a9102dc1a43bc716e352342353089b27
Andrew Ford <andrew@bountysource.com>
2014-06-03 17:24:12 -0700

    Remove logic from teamView

--- src/app/teams/team_badge_controller.js
+++ src/app/teams/team_badge_controller.js
@@ -3,11 +3,16 @@
 angular.module('teams').controller('TeamBadgeController', function ($rootScope, $scope, $modal, TeamBadgeBountiesPosted, TeamBadgeBountiesReceived, TeamBadgeRaised) {
 
   var $parentScope = $scope;
-
-  $scope.badgeBountiesTotal = new TeamBadgeBountiesReceived($scope.team);
-
-  $scope.badgeFundingTotal = new TeamBadgeRaised($scope.team);
-
+  
+  $scope.$watch('fundraisers', function (fundraisers) {
+    if (angular.isArray(fundraisers)) {
+      if ($scope.fundraisers.length > 0) {
+      $scope.badge = new TeamBadgeRaised($scope.team);
+      } else {
+      $scope.badge = new TeamBadgeBountiesReceived($scope.team);  
+      }
+    }
+  });
 
   $scope.showBadgeModal = function () {
     $modal.open({
