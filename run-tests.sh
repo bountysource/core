55969e068843131f5aa6966b4d53ab2a63321ba0
Fernando Cordeiro <fernando@fernandocordeiro.tech>
2017-03-19 06:43:39 -0300

    Tests in tasks and Docs in Wiki

--- run-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-/app/bin/rake db:drop db:create spec:prepare
-/app/bin/bundle exec rspec --pattern spec/models{,/**}/*_spec.rb
-/app/bin/rake db:drop db:create spec:prepare
-/app/bin/bundle exec rspec --pattern spec/trackers{,/**}/*_spec.rb
-/app/bin/rake db:drop db:create spec:prepare
-/app/bin/bundle exec rspec --pattern spec/{controllers,features,helpers,mailers,routing}{,/**}/*_spec.rb
-/app/bin/rake jshint
