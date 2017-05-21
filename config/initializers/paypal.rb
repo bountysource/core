106775f96214fc2ba390f510e94edc61c545ae5a
Alpha Shuro <alphashuro@gmail.com>
2017-05-21 20:20:03 +0200

    use env vars for paypal config

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,2 +1,6 @@
-PayPal::SDK.load("config/paypal.yml", Rails.env)
+PayPal::SDK.configure(
+  :mode => ENV['PAYPAL_SANDBOX']=='true' ? 'sandbox' : 'live', # "sandbox" or "live"
+  :client_id => ENV['PAYPAL_CLIENT_ID'],
+  :client_secret => ENV['PAYPAL_CLIENT_SECRET'],
+  :ssl_options => { } )
 PayPal::SDK.logger = Rails.logger
