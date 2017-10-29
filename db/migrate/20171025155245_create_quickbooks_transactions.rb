cc664a368e4845512db8686bf841b68b197c7090
Warren Konkel <wkonkel@gmail.com>
2017-10-28 22:53:59 -0700

    Quickbooks migrations

---
+++ db/migrate/20171025155245_create_quickbooks_transactions.rb
@@ -0,0 +1,13 @@
+class CreateQuickbooksTransactions < ActiveRecord::Migration
+  def up
+    create_table :quickbooks_transactions, id: false do |t|
+      t.integer :id
+      t.timestamps
+    end
+    execute "ALTER TABLE quickbooks_transactions ADD PRIMARY KEY (id);"
+  end
+
+  def down
+    drop_table :quickbooks_transactions
+  end
+end
