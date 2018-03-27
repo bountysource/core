f79b279d915a3ded68f2c6cb1e163a57ce20e711
Warren Konkel <wkonkel@gmail.com>
2018-03-27 11:49:01 -0700

    Remove `alias_method_chain`
    • No longer support "legacy passwords". Users will need to perform a password reset going forward.

--- spec/support/default_vendor_params.rb
+++ spec/support/default_vendor_params.rb
@@ -1,19 +1,12 @@
 require 'active_support/concern'
 
 module DefaultVendorParams
-  extend ActiveSupport::Concern
-
-  def process_with_vendor_default(action, http_method, params={}, session={}, flash={})
-    params = default_params.merge(params)
-    process_without_vendor_default(action, http_method, params, session, flash)
-  end
-
-  included do
-    let(:default_params) { {vendor_string: 'bountysource'} }
-    alias_method_chain :process, :vendor_default
+  def process(action, http_method, params={}, session={}, flash={})
+    params = { vendor_string: 'bountysource' }.merge(params)
+    super(action, http_method, params, session, flash)
   end
 end
 
 RSpec.configure do |config|
-  config.include(DefaultVendorParams, :type => :controller)
+  config.prepend(DefaultVendorParams, :type => :controller)
 end
