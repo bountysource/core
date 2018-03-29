ddd716b266b02b0ee785ea26cc759c4ff58683cb
Jalen Ong <jalen.ong@gmail.com>
2018-03-28 23:01:54 -0700

    Fix linked account to facebook

--- config/initializers/has_cloudinary_image.rb
+++ config/initializers/has_cloudinary_image.rb
@@ -24,7 +24,6 @@ class ApplicationRecord
             self.cloudinary_id = "upload/#{image}"
           else
             # skip if no new input and image already exists
-            return
           end
         elsif input =~ /github/
           self.cloudinary_id = input
