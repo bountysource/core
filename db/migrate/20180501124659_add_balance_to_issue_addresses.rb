ca4850d6cd10f97ef094e806e6f3c6b2389d03eb
You Jing Wong <wongyoujing118@gmail.com>
2018-05-01 20:57:58 +0800

    add migrations

--- db/migrate/20180501124659_add_balance_to_issue_addresses.rb
+++ db/migrate/20180501124659_add_balance_to_issue_addresses.rb
@@ -1,6 +1,6 @@
 class AddBalanceToIssueAddresses < ActiveRecord::Migration[5.1]
   def change
-    add_column :issues_addresses, :balance_updated_at, :datetime
-    add_column :issues_addresses, :balance, :jsonb
+    add_column :issue_addresses, :balance_updated_at, :datetime
+    add_column :issue_addresses, :balance, :jsonb
   end
 end
