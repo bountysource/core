1faacf06ae10db7daa1c3bb91679f0cad549764d
Jalen Ong <jalen.ong@gmail.com>
2018-05-04 02:18:52 -0700

    Remove banner (#1326)

--- app/assets/javascripts/app/layout/navbar.js
+++ app/assets/javascripts/app/layout/navbar.js
@@ -113,14 +113,6 @@ angular.module('app').controller('NavbarLinkedAccountSignin', function($scope, $
   };
 });
 
-angular.module('app').controller('CanyaAnnouncementController', function ($scope, $cookieJar) {
-  $scope.hideCanyaAnnouncement = function() {
-    $scope.canyaAnnouncementIsVisible = false;
-    $cookieJar.setJson('hide_canya_testing', true);
-  };
-  $scope.canyaAnnouncementIsVisible = !$cookieJar.getJson('hide_canya_testing');
-});
-
 angular.module('app').controller('BancorWidgetController', function ($rootScope, $scope, $location) {
   $scope.visible = false;
   $scope.close = function(){
