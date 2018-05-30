1721479dc1f42928594a894af852b2c2fc2321a8
You Jing Wong <wongyoujing118@gmail.com>
2018-05-30 09:23:50 +0800

    fix migration

--- db/migrate/20180530004731_add_unique_index_to_issue_address_and_bounties.rb
+++ db/migrate/20180530004731_add_unique_index_to_issue_address_and_bounties.rb
@@ -1,5 +1,6 @@
 class AddUniqueIndexToIssueAddressAndBounties < ActiveRecord::Migration[5.1]
   def change
+    remove_index :issue_addresses, :public_address
     add_index :issue_addresses, :public_address, unique: true
     add_index :crypto_bounties, :transaction_hash, unique: true
   end
