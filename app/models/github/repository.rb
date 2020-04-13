12b66b061f01761d28192963f6c3d8e3ab2da6e7
Alpha Shuro <alphashuro@gmail.com>
2020-04-13 12:52:30 +0200

    run remote sync only if not already queued

--- app/models/github/repository.rb
+++ app/models/github/repository.rb
@@ -111,13 +111,14 @@ class Github::Repository < Tracker
       update_attributes!(sync_in_progress: false)
     end
   rescue Github::API::RateLimitExceeded => error
-    # if not, queue to run after the rate limit expires
-    rate_limit_resets_at = error.response.headers['x-ratelimit-reset'][0]
-    reset_time = Time.strptime(rate_limit_resets_at, "%s")
+    # only queue remote sync job if it's not already queued
+    unless sync_in_progress
+      # queue it to run after the rate limit expires
+      rate_limit_resets_at = error.response.headers['x-ratelimit-reset'][0]
+      reset_time = Time.strptime(rate_limit_resets_at, "%s")
 
-    delay(run_at: reset_time, priority: 150).remote_sync(options)
-  rescue => e
-    puts(e)
+      delay(run_at: reset_time, priority: 150).remote_sync(options)
+    end
   end
 
   # make the actual API call and update the model. needs to be it's own method for message queueing
