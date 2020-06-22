50a3de8fef4bdb44c7fb62475985d1ef36b08aca
Alpha <alphashuro@gmail.com>
2020-06-22 10:52:53 +0200

    disable rabl cache

--- config/initializers/rabl.rb
+++ config/initializers/rabl.rb
@@ -144,7 +144,8 @@ Rabl.configure do |config|
   config.include_json_root = false
   config.include_child_root = false
   config.enable_json_callbacks = false
-  config.cache_sources = Rails.env != 'development'
+  config.cache_sources = false
+  #config.cache_sources = Rails.env != 'development'
   #config.cache_all_output = Rails.env != 'development'
 
   config.pagination = {
