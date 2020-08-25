13c8c70ca32086127af8c03339ee9ce16f35aff3
root <dev.mred@gmail.com>
2020-08-25 17:15:52 +0200

    Update some tests

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,11 @@ end
 module Api
   class Application < Rails::Application
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head]
+
     # All times should be in UTC
     ENV['TZ'] = 'UTC'
     config.time_zone = 'UTC'
