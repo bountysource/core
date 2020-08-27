79441de3efff90890cd3eac698a6422f4bf3d017
Mourad <dev.mred@gmail.com>
2020-08-27 10:50:43 +0200

    test

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,23 @@
 # config/unicorn.rb
+port ENV['PORT'] || 3000
+environment ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
+workers Integer(ENV['PUMA_WORKERS'] || ENV['WEB_CONCURRENCY'] || 0)
+threads_count = Integer(ENV['PUMA_THREADS'] || ENV['RAILS_MAX_THREADS'] || 1)
+threads threads_count, threads_count
+
+quiet
+
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
+
+rackup      DefaultRackup
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ApplicationRecord.establish_connection
+end
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
@@ -21,4 +37,7 @@ after_fork do |server, worker|
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
 end
+
+
+
 
