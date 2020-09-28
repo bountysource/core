244d1d4544e39a9d2d8d4e569ca5f1b7744003bd
root <dev.mred@gmail.com>
2020-09-28 10:01:44 +0200

    Create pagination and all teams view page Frontend

--- app/assets/javascripts/app/teams/index.js
+++ app/assets/javascripts/app/teams/index.js
@@ -1,38 +1,72 @@
-angular.module('app').controller('TeamsIndexController', function ($scope, $location, $routeParams, $anchorScroll, $api, $pageTitle, Team, Tag) {
-  $pageTitle.set("Teams");
-
-  $scope.tags = Tag.query({ featured: true, per_page: 250 });
-
-  $scope.set_params = function(params) {
-    var options = {
+angular
+  .module("app")
+  .controller("TeamsIndexController", function (
+    $scope,
+    $location,
+    $routeParams,
+    $anchorScroll,
+    $api,
+    $pageTitle,
+    Team,
+    Tag
+  ) {
+    var defaultOptions = {
       include_bio: true,
-      per_page: 25
+      per_page: 25,
+      page: 1,
     };
-
-    if (!params.tag_id) {
-      options.featured = true;
-      $scope.selected = 'featured';
-    } else if ((""+params.tag_id).match(/^\d+$/)) {
-      options.tag_child_id = parseInt(params.tag_id);
-      options.tag_child_type = 'Tag';
-      $scope.team_self = null;
-      $scope.selected = 'tag:'+options.tag_child_id;
-    } else {
-      options.tag_child_id = params.tag_id;
-      options.tag_child_type = 'TeamSlug';
-      $scope.team_self = Team.get({ slug: params.tag_id });
-      $scope.selected = 'team:'+options.tag_child_id;
+    function initalLoad() {
+      $pageTitle.set("Teams");
+      $scope.tags = Tag.query({ featured: true, per_page: 250 });
+      $scope.currentPage = 1;
+      $scope.perPage = defaultOptions.per_page;
+      $scope.set_params({
+        tag_id: $routeParams.tag_id,
+      });
     }
-
-    console.log(options);
-    $scope.teams = Team.query(options);
-
-    $anchorScroll();
-  };
-
-  // initial page load
-  $scope.set_params({
-    tag_id: $routeParams.tag_id
+    $scope.changePage = function (page) {
+      defaultOptions.page = page;
+      defaultOptions.pagination = true;
+      $api.v2.teamsPagination(defaultOptions).then((response) => {
+        $scope.currentPage = page;
+        $scope.totalItems = parseInt(response.headers()["total-items"], 10);
+        $scope.teams = response.data;
+      });
+    };
+ 
+    $scope.set_params = function (params) {
+      var options = {
+        include_bio: true,
+        per_page: 25,
+      };
+      if (params.all) {
+        $scope.selected = "all";
+        $scope.currentPage = 1;
+        defaultOptions.page = 1;
+        defaultOptions.pagination = true;
+        $api.v2.teamsPagination(defaultOptions).then((response) => {
+          $scope.totalItems = parseInt(response.headers()["total-items"], 10);
+          $scope.teams = response.data;
+        });
+      } else {
+        if (!params.tag_id) {
+          options.featured = true;
+          $scope.selected = "featured";
+        } else if (("" + params.tag_id).match(/^\d+$/)) {
+          options.tag_child_id = parseInt(params.tag_id);
+          options.tag_child_type = "Tag";
+          $scope.team_self = null;
+          $scope.selected = "tag:" + options.tag_child_id;
+        } else {
+          options.tag_child_id = params.tag_id;
+          options.tag_child_type = "TeamSlug";
+          $scope.team_self = Team.get({ slug: params.tag_id });
+          $scope.selected = "team:" + options.tag_child_id;
+        }
+        $scope.teams = Team.query(options);
+        $anchorScroll();
+      }
+    };
+ 
+    initalLoad();
   });
-
-});
